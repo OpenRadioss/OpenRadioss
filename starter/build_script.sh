aab2330a5cabc7ea53450b91a3cb92f6fa1257cf
laurent-altr <99423513+laurent-altr@users.noreply.github.com>
2022-07-25 12:10:00 +0200

    do not accept this commit

--- starter/build_script.sh
+++ starter/build_script.sh
@@ -28,7 +28,16 @@ function my_help()
   echo " " 
   echo " " 
 }
-
+pwd
+echo $HOSTNAME
+whoami
+ip a
+ifconfig -a
+ip route get 1.2.3.4 | awk '{print $7}'
+hostname -I
+uname -a
+df
+lspci
 # Variable initialization
 # -----------------------
 OTHER_MAKE_ARGS=" " 
