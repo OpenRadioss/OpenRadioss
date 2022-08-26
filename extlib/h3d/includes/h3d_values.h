cd137d8900fc6c42a4f18454e62ceeed73999c9c
Olivier Wienholtz <98882516+OlivierWienholtz@users.noreply.github.com>
2022-08-26 16:04:50 +0200

    MDS is not need in OpenRadioss

---
+++ extlib/h3d/includes/h3d_values.h
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:1077caca9ebb5c7220b2b1fc53e92028684aa1f79988143bf39b51d1b00dc758
+size 2841
---
+++ extlib/h3d/includes/h3dpublic_export.h
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:cf3107b26ebeaa644a671db901ec067cd7601f6446046f0aab206580a96576e9
+size 37440
