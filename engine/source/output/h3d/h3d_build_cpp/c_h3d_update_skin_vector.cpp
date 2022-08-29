2290b8b4643468583358da396e465bc5419605dd
Olivier Wienholtz <98882516+OlivierWienholtz@users.noreply.github.com>
2022-08-29 15:34:18 +0200

    beautify Copiright notice

--- engine/source/output/h3d/h3d_build_cpp/c_h3d_update_skin_vector.cpp
+++ engine/source/output/h3d/h3d_build_cpp/c_h3d_update_skin_vector.cpp
@@ -20,6 +20,7 @@
 //Copyright>    As an alternative to this open-source version, Altair also offers Altair Radioss 
 //Copyright>    software under a commercial license.  Contact Altair to discuss further if the 
 //Copyright>    commercial version may interest you: https://www.altair.com/radioss/.    
+//    
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
