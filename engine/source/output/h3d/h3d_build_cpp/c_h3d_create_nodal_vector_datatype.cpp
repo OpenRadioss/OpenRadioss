db7335108a0fe724e9ed700bcfcd03c3b3bc6f67
SebastienVilleneuve <100404030+sebastienVilleneuve@users.noreply.github.com>
2023-01-30 11:51:39 +0100

    h3d correction possible overflow

--- engine/source/output/h3d/h3d_build_cpp/c_h3d_create_nodal_vector_datatype.cpp
+++ engine/source/output/h3d/h3d_build_cpp/c_h3d_create_nodal_vector_datatype.cpp
@@ -93,19 +93,6 @@ void c_h3d_create_nodal_vector_datatype_(int *cpt_data, char *name, int *size, i
     for(i=0;i<*s_comment;i++)  ccomment[i] = comment[i];
     ccomment[*s_comment]='\0';  
 
-    char * LAYERPOOL = new char [*size1+11];
-    H3D_ID layer_pool_id = H3D_NULL_ID;
-    if(*info2 != 0)
-    {
-        sprintf(LAYERPOOL, "%s %d %d" ,cname1,*info1,*info2);
-        rc = Hyper3DAddString(h3d_file, LAYERPOOL, &layer_pool_id);
-    }
-    else
-    {
-        sprintf(LAYERPOOL, "%s %d" ,cname1,*info1);
-        rc = Hyper3DAddString(h3d_file, LAYERPOOL, &layer_pool_id);
-    }
-
 
     char edata_type[50];
 
@@ -131,14 +118,7 @@ void c_h3d_create_nodal_vector_datatype_(int *cpt_data, char *name, int *size, i
              if( !rc ) throw rc;
         }
  
-        if(*info1 != 0 && *cpt_data != 0)
-        {
-            rc = Hyper3DDatatypePools(h3d_file, *cpt_data , node_poolname_id, 1, 
-                                    &layer_pool_id, has_corners, tensor_type, poisson);
-            if( !rc ) throw rc;
-
-        }
-        else if (*cpt_data != 0)
+        if (*cpt_data != 0)
         {
             rc = Hyper3DDatatypePools(h3d_file, *cpt_data , node_poolname_id, 0, 
                                     layername_ids, has_corners, tensor_type, poisson);
@@ -154,7 +134,6 @@ void c_h3d_create_nodal_vector_datatype_(int *cpt_data, char *name, int *size, i
     catch(...) {
         Hyper3DExportClearError(h3d_file);
     }
-    delete [] LAYERPOOL;
     free(cname);
     free(cname1);
     free(ccomment);
