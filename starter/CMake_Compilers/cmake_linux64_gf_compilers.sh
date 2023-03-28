76294da49d3968ae84048d2bb67e44ad658411fd
Olivier Wienholtz <98882516+OlivierWienholtz@users.noreply.github.com>
2023-03-28 11:41:58 +0200

    Issue in Build system : Random behave

--- starter/CMake_Compilers/cmake_linux64_gf_compilers.sh
+++ starter/CMake_Compilers/cmake_linux64_gf_compilers.sh
@@ -1,5 +1,5 @@
 # Fixed compiler switches for linux64 Gfortran binary
-Fortran_comp="gfortran"
-C_comp="gcc"
-CXX_comp="g++"
-CPP_comp="g++"
+Fortran_comp=gfortran
+C_comp=gcc
+CXX_comp=g++
+CPP_comp=g++
--- starter/CMake_Compilers/cmake_win64_compilers.sh
+++ starter/CMake_Compilers/cmake_win64_compilers.sh
@@ -1,4 +1,4 @@
-Fortran_comp="ifx.exe"
-C_comp="icx.exe"
-CXX_comp="icx.exe"
-CPP_comp="icx.exe"
+Fortran_comp=ifx.exe
+C_comp=icx.exe
+CXX_comp=icx.exe
+CPP_comp=icx.exe
