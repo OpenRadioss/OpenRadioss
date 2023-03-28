1d867c6603c3d8c8d97a3c5751f0fa43867d25b0
Olivier Wienholtz <98882516+OlivierWienholtz@users.noreply.github.com>
2023-03-28 13:33:39 +0200

    Windows Issue in cmake_win64.txt and new build scrips cmake_win64.txt : with a3d293ab964b429f5731dd5fe2b7b33a83bad842 link method changed Stacksize setting was no more taken into account.
    Create Windows native batch scripts for Starter and Engine to compile Radioss out of cygwin.

---
+++ starter/build_windows.bat
@@ -0,0 +1,136 @@
+echo OFF
+
+REM Variable setting
+set arch=win64
+set prec=dp
+set debug=0
+set static=0
+set verbose=
+set clean=0
+set jobs=1
+set jobsv=1
+
+IF (%1) == () GOTO ERROR
+
+:ARG_LOOP
+IF (%1) == () GOTO END_ARG_LOOP
+
+   IF %1==-prec (
+       set prec=%2 
+    )
+
+   IF %1==-debug (
+       set debug=%2
+   )
+
+   IF %1==-static-link (
+       set static=1
+   )
+
+   IF %1==-verbose (
+       set verbose=-v
+   )
+
+   IF %1==-clean (
+       set clean=1
+   )
+
+   IF %1==-nt (
+       set jobs=%2
+       set jobsv=%2
+       )
+   )
+
+SHIFT
+GOTO ARG_LOOP
+
+:END_ARG_LOOP
+
+
+if %jobsv%==all ( set jobs=0)
+
+
+Rem Starter name
+if %prec%==sp ( set sp_suffix=_sp)
+if %debug%==1 ( set debug_suffix=_db)
+if %debug%==2 ( set debug_suffix=_db)
+
+set starter=starter_%arch%%sp_suffix%%debug_suffix%.exe
+
+Rem Create build directory
+
+
+set build_directory=cbuild_%arch%%sp_suffix%%debug_suffix%_ninja
+
+Ren clean
+if %clean%==1 (
+  echo.
+  echo Cleaning %build_directory%
+  RMDIR /S /Q %build_directory%
+  goto END
+)
+
+echo.
+echo Build OpenRadioss Starter
+echo --------------------------
+echo.
+echo  Build Arguments :
+echo  arch =                 : %arch%
+echo  precision =            : %prec%
+echo  debug =                : %debug%
+echo  static_link =          : %static_link%
+echo.
+echo  Running on             : %jobsv% Threads
+echo.
+echo  verbose=               : %verbose%
+echo.
+echo  Build directory:  %build_directory%
+echo.
+
+if exist %build_directory% (
+
+  cd  %build_directory%
+
+) else (
+
+  mkdir %build_directory%
+  cd  %build_directory%
+)
+
+Rem Load Compiler settings
+call ..\CMake_Compilers\cmake_%arch%_compilers.bat
+
+
+
+cmake -G Ninja -Darch=%arch% -Dprecision=%prec% -Ddebug=%debug%  -Dstatic_link=%static% -DCMAKE_BUILD_TYPE=Release -DCMAKE_Fortran_COMPILER=%Fortran_comp% -DCMAKE_C_COMPILER=%C_comp% -DCMAKE_CPP_COMPILER=%CPP_comp% -DCMAKE_CXX_COMPILER=%CXX_comp% ..
+ninja %verbose% -j %jobs%
+
+if exist %starter% (
+  echo.
+  echo Copy %starter% in exec directory
+  copy %starter% ..\..\exec
+)
+
+cd ..
+
+GOTO END
+
+:ERROR
+
+  echo Use with arguments : 
+  echo     -arch=[build architecture]          : set architecture : default  Windows 64 bit
+  echo     -prec=[dp,sp]                       : set precision - dp (default),sp
+  echo     -static-link                        : Compiler runtime is linked in binary
+  echo     -debug=[0,1]                        : debug version 0 no debug flags (default), 1 usual debug flag )
+  echo.
+  echo Execution control 
+  echo     -nt [N,all]        : Run build with N Threads, all : takes all ressources of machine
+  echo     -verbose           : Verbose build
+  echo     -clean             : clean build directory
+  echo.
+
+:END
+echo.
+echo Terminating
+echo.
+
