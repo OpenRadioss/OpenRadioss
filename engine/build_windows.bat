2c9bb3fc8b78ce8414e36faadfe29ec31fc0feff
Olivier Wienholtz <98882516+OlivierWienholtz@users.noreply.github.com>
2023-11-25 19:03:07 +0100

    Several changes for builds: 1/ Add f90 Free format support in Starter & Engine builds 2/ Crete new build _asan : Gfortran Address Sanitizer 3/ Create Windows builds with Debug/Check bounds    Add them in Visual Studio 2022 menus 4/ Defaults for Linux64_gf is now asan    Default for Windows is now chkb

--- engine/build_windows.bat
+++ engine/build_windows.bat
@@ -5,7 +5,8 @@ set arch=win64
 set dc=
 set dc_suf=
 set prec=dp
-set debug=0
+set debug=chkb
+set release=0
 set static=0
 set MPI="-DMPI=smp"
 set pmpi=SMP
@@ -33,6 +34,10 @@ IF (%1) == () GOTO END_ARG_LOOP
 
    IF %1==-debug (
        set debug=%2
+   )
+
+   IF %1==-release (
+       set release=1
    )
 
    IF %1==-static-link (
@@ -76,8 +81,25 @@ if %jobsv%==all ( set jobs=0)
 
 Rem Engine name
 if %prec%==sp   ( set sp_suffix=_sp)
-if %debug%==1   ( set debug_suffix=_db)
-if %debug%==2   ( set debug_suffix=_db2)
+
+if %debug%==0 (
+    set debug_suffix=''
+) else (
+
+   if %debug%==1 (
+    set debug_suffix='_db'
+   ) else (
+    set debug_suffix=_%debug%
+   )
+
+)
+
+Rem if release is set, set debug to zero and no suffix
+if  %release%==1 (
+    set debug_suffix=
+    set debug=0
+)
+
 if %got_mpi%==1 ( set mpi_suffix=_%pmi%)
 
 if %cbuild%==0 (
@@ -164,11 +186,15 @@ GOTO END
   echo     -arch=[build architecture]          : set architecture : default  Windows 64 bit
   echo           -arch=win64                     (SMP executable / Intel OneAPI / Windows X86-64)
   echo           -arch=win64       -mpi=impi     (Intel MPI OneAPI executable / Intel OneAPI / Windows X86-64)
-  echo .
+  echo.
   echo     -mpi=[smp,impi]                     : set MPI version
   echo     -prec=[dp,sp]                       : set precision - dp (default),sp
   echo     -static-link                        : Compiler runtime is linked in binary
-  echo     -debug=[0,1]                        : debug version 0 no debug flags (default), 1 usual debug flag )
+  echo     -debug=[0,1,chkb]                   : debug version 
+  echo                                              0: no debug flags
+  echo                                              1: usual debug flag
+  echo                                              chkb: check bounds build (default)
+  echo     -release                            : set Release build.
   echo.
   echo Execution control 
   echo     -nt [N,all]        : Run build with N Threads, all : takes all ressources of machine
