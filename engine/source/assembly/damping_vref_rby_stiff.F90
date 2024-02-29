796f1e68667d030f9763a98571010261a091deee
bertrand-altair <103751412+bertrand-altair@users.noreply.github.com>
2024-02-29 14:34:34 +0100

    /DAMP/VREL - SPMD fixes + SMP fixes + stability fixes

---
+++ engine/source/assembly/damping_vref_rby_stiff.F90
@@ -0,0 +1,93 @@
+!Copyright>        OpenRadioss
+!Copyright>        Copyright (C) 1986-2024 Altair Engineering Inc.
+!Copyright>
+!Copyright>        This program is free software: you can redistribute it and/or modify
+!Copyright>        it under the terms of the GNU Affero General Public License as published by
+!Copyright>        the Free Software Foundation, either version 3 of the License, or
+!Copyright>        (at your option) any later version.
+!Copyright>
+!Copyright>        This program is distributed in the hope that it will be useful,
+!Copyright>        but WITHOUT ANY WARRANTY; without even the implied warranty of
+!Copyright>        MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+!Copyright>        GNU Affero General Public License for more details.
+!Copyright>
+!Copyright>        You should have received a copy of the GNU Affero General Public License
+!Copyright>        along with this program.  If not, see <https://www.gnu.org/licenses/>.
+!Copyright>
+!Copyright>
+!Copyright>        Commercial Alternative: Altair Radioss Software
+!Copyright>
+!Copyright>        As an alternative to this open-source version, Altair also offers Altair Radioss
+!Copyright>        software under a commercial license.  Contact Altair to discuss further if the
+!Copyright>        commercial version may interest you: https://www.altair.com/radioss/.
+      module damping_vref_rby_stiff_mod
+      contains
+! ======================================================================================================================
+!                                                   PROCEDURES
+! ======================================================================================================================
+!
+!=======================================================================================================================
+!!\brief This subroutine computes nodal stiffness on main nodes of RBODY for /DAMP/VREL
+!=======================================================================================================================
+!
+        subroutine damping_vref_rby_stiff(numnod,nnpby,nrbykin,nrbykin_l,npby,                       &
+                                          rby6_c,ms,in,stifn,stifr,size_rby6_c)
+! ----------------------------------------------------------------------------------------------------------------------
+!                                                   Modules
+! ----------------------------------------------------------------------------------------------------------------------
+          use constant_mod , only: one,two,em20
+! ----------------------------------------------------------------------------------------------------------------------
+!                                                   Implicit none
+! ----------------------------------------------------------------------------------------------------------------------
+          implicit none
+! ----------------------------------------------------------------------------------------------------------------------
+!                                                   Included files
+! ----------------------------------------------------------------------------------------------------------------------
+#include "my_real.inc"       
+! ----------------------------------------------------------------------------------------------------------------------
+!                                                   Arguments
+! ----------------------------------------------------------------------------------------------------------------------
+          integer,                                   intent(in) :: numnod                      !< number of nodes         
+          integer,                                   intent(in) :: nnpby                       !< first dimension of array NPBY
+          integer,                                   intent(in) :: nrbykin                     !< number of rigid bodies
+          integer,                                   intent(in) :: nrbykin_l                   !< number of rigid bodies on this proc         
+          integer,                                   intent(in) :: npby(nnpby,nrbykin)         !< main structure for rigid bodies
+          integer,                                   intent(in) :: size_rby6_c                 !< dimension of array rby6c 
+          my_real,                                   intent(in) :: ms(numnod)                  !< nodal mass
+          my_real,                                   intent(in) :: in(numnod)                  !< nodal inertia      
+          my_real,                                intent(inout) :: stifn(numnod)               !< nodal stiffness
+          my_real,                                intent(inout) :: stifr(numnod)               !< nodal rotational stiffness                                  
+          double precision,                          intent(in) :: rby6_c(2,6,size_rby6_c)         !< working array for rigid body damping assembly  
+! ----------------------------------------------------------------------------------------------------------------------
+!                                                   Local variables
+! ----------------------------------------------------------------------------------------------------------------------
+          integer :: nd,m
+          my_real :: c_tot,cr_tot,dd,fac
+! ----------------------------------------------------------------------------------------------------------------------
+!                                                   Body
+! ----------------------------------------------------------------------------------------------------------------------        
+!         
+          do nd=1,nrbykin_l
+
+            if(npby(7,nd)>0) THEN
+!
+              m = npby(1,nd)
+
+              c_tot = rby6_c(1,1,nd)+rby6_c(1,2,nd)+rby6_c(1,3,nd)+rby6_c(1,4,nd)+rby6_c(1,5,nd)+rby6_c(1,6,nd)
+              cr_tot= rby6_c(2,1,nd)+rby6_c(2,2,nd)+rby6_c(2,3,nd)+rby6_c(2,4,nd)+rby6_c(2,5,nd)+rby6_c(2,6,nd)
+!            
+              dd = c_tot/sqrt(two*stifn(m)*ms(m))
+              fac = sqrt(one + dd*dd) - dd
+              stifn(m) = stifn(m) / fac**2
+!
+              dd = cr_tot/sqrt(two*stifr(m)*in(m))
+              fac = sqrt(one + dd*dd) - dd
+              stifr(m) = stifr(m) / fac**2
+!              
+            endif  
+!
+          enddo          
+!
+! ----------------------------------------------------------------------------------------------------------------------
+        end subroutine damping_vref_rby_stiff
+      end module damping_vref_rby_stiff_mod
---
+++ starter/source/general_controls/damping/damping_rby_spmdset.F90
@@ -0,0 +1,80 @@
+!Copyright>        OpenRadioss
+!Copyright>        Copyright (C) 1986-2024 Altair Engineering Inc.
+!Copyright>
+!Copyright>        This program is free software: you can redistribute it and/or modify
+!Copyright>        it under the terms of the GNU Affero General Public License as published by
+!Copyright>        the Free Software Foundation, either version 3 of the License, or
+!Copyright>        (at your option) any later version.
+!Copyright>
+!Copyright>        This program is distributed in the hope that it will be useful,
+!Copyright>        but WITHOUT ANY WARRANTY; without even the implied warranty of
+!Copyright>        MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+!Copyright>        GNU Affero General Public License for more details.
+!Copyright>
+!Copyright>        You should have received a copy of the GNU Affero General Public License
+!Copyright>        along with this program.  If not, see <https://www.gnu.org/licenses/>.
+!Copyright>
+!Copyright>
+!Copyright>        Commercial Alternative: Altair Radioss Software
+!Copyright>
+!Copyright>        As an alternative to this open-source version, Altair also offers Altair Radioss
+!Copyright>        software under a commercial license.  Contact Altair to discuss further if the
+!Copyright>        commercial version may interest you: https://www.altair.com/radioss/.
+      module damping_rby_spmdset_mod
+      contains
+! ======================================================================================================================
+!                                                   PROCEDURES
+! ======================================================================================================================
+!
+!=======================================================================================================================
+!\brief This subroutine stick main node of ridid body on procs where damping nodes are present
+!=======================================================================================================================
+!
+        subroutine damping_rby_spmdset(igrnod,ngrnod,ndamp,nrdamp,dampr,nnpby,nrbykin,npby)
+! ----------------------------------------------------------------------------------------------------------------------
+!                                                   Modules
+! ----------------------------------------------------------------------------------------------------------------------
+          use GROUPDEF_MOD , only: GROUP_
+! ----------------------------------------------------------------------------------------------------------------------
+!                                                   Implicit none
+! ----------------------------------------------------------------------------------------------------------------------
+          implicit none
+! ----------------------------------------------------------------------------------------------------------------------
+!                                                   Included files
+! ----------------------------------------------------------------------------------------------------------------------
+#include "my_real.inc"       
+! ----------------------------------------------------------------------------------------------------------------------
+!                                                   Arguments
+! ----------------------------------------------------------------------------------------------------------------------
+          type(GROUP_),                              intent(in) :: igrnod(ngrnod)              !< group od nodes structure
+          integer,                                   intent(in) :: ngrnod                      !< number of groups of nodes 
+          integer,                                   intent(in) :: ndamp                       !< number of /DAMP
+          integer,                                   intent(in) :: nrdamp                      !< first dimension of array DAMP       
+          integer,                                   intent(in) :: nnpby                       !< first dimension of array NPBY
+          integer,                                   intent(in) :: nrbykin                     !< number of rigid bodies
+          integer,                                   intent(in) :: npby(nnpby,nrbykin)         !< main structure for rigid bodies                    
+          my_real,                                   intent(in) :: dampr(nrdamp,ndamp)         !< main structure for option /DAMP
+! ----------------------------------------------------------------------------------------------------------------------
+!                                                   Local variables
+! ----------------------------------------------------------------------------------------------------------------------
+          integer :: nd,igr,isk,id_rby
+! ----------------------------------------------------------------------------------------------------------------------
+!
+! ----------------------------------------------------------------------------------------------------------------------
+!                                                   Body
+! ----------------------------------------------------------------------------------------------------------------------         
+!         
+          do nd=1,ndamp
+!
+            id_rby = nint(dampr(25,nd))
+!         
+            if (id_rby > 0) then        
+              igr   = nint(dampr(2,nd))
+              call spmdset(id_rby,npby,nnpby,igrnod(igr)%entity,igrnod(igr)%nentity,0)              
+            endif
+!
+          enddo    
+!
+! ----------------------------------------------------------------------------------------------------------------------
+        end subroutine damping_rby_spmdset
+      end module damping_rby_spmdset_mod
