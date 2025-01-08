5ffdecf1dbb091190d2e46b191be327fe6adbfc0
OlivierWienholtz <98882516+OlivierWienholtz@users.noreply.github.com>
2025-01-08 08:40:33 +0100

    Adjust cmake build headers for IntelOneAPI 2025.0.1

--- engine/source/user_interface/eng_callback_c.c
+++ engine/source/user_interface/eng_callback_c.c
@@ -52,33 +52,32 @@ void sav_buf_point_(int *buf, int *i)
 }
 
 
-void _FCALL SAV_BUF_POINT(buf,i)
-int *buf ,  *i;{sav_buf_point_(buf,i);}
-void sav_buf_point(buf,i)
-int *buf ,  *i;{sav_buf_point_(buf,i);}
+void _FCALL SAV_BUF_POINT(int *buf,int *i)
+{sav_buf_point_(buf,i);}
+
+void sav_buf_point(int *buf,int *i)
+{sav_buf_point_(buf,i);}
 
 
-void sav_buf_point__(buf,i)
-int *buf ,  *i;{sav_buf_point_(buf,i);}
+void sav_buf_point__(int *buf,int *i)
+{sav_buf_point_(buf,i);}
 
 
-void get_buf_point_(loca,ish,i)
-int *loca ; int *ish ,  *i;{*ish = (sav_buf[*i] - loca);}
+void get_buf_point_(int *loca,int *ish,int *i)
+{*ish = (sav_buf[*i] - loca);}
 
-void _FCALL GET_BUF_POINT(loca,ish,i)
-int *loca ; int *ish ,  *i;{*ish = (sav_buf[*i] - loca);}
+void _FCALL GET_BUF_POINT(int *loca,int *ish,int *i)
+{*ish = (sav_buf[*i] - loca);}
 
-void get_buf_point(loca,ish,i)
-int *loca ; int *ish ,  *i;{*ish = (sav_buf[*i] - loca);}
-
-
-void get_buf_point__(loca,ish,i)
-int *loca ; int *ish ,  *i;{*ish = (sav_buf[*i] - loca);}
+void get_buf_point(int *loca,int *ish,int *i)
+{*ish = (sav_buf[*i] - loca);}
 
 
-void get_var_user_f_ (buf,decalage,resultat)
-int * buf, * decalage;
-double * resultat;
+void get_buf_point__(int *loca,int *ish,int *i)
+{*ish = (sav_buf[*i] - loca);}
+
+
+void get_var_user_f_ (int *buf,int *decalage,double * resultat)
 {
  double *res;
    res= ((double*) (sav_buf[*buf]) )+ (*decalage -1);
@@ -86,25 +85,19 @@ double * resultat;
 }
 
 
-void get_var_user_f(buf,decalage,resultat)
-int * buf, * decalage;double * resultat;
+void get_var_user_f(int *buf,int *decalage,double *resultat)
 {get_var_user_f_(buf,decalage,resultat);}
 
 
-void _FCALL GET_VAR_USER_F(buf,decalage,resultat)
-int * buf, * decalage;double * resultat;
+void _FCALL GET_VAR_USER_F(int *buf,int *decalage,double *resultat)
 {get_var_user_f_(buf,decalage,resultat);}
 
 
-void get_var_user_f__(buf,decalage,resultat)
-int * buf, * decalage;double * resultat;
+void get_var_user_f__(int *buf,int *decalage,double *resultat)
 {get_var_user_f_(buf,decalage,resultat);}
 
 
-void get_var_user_f_sp_ (buf,decalage,resultat)
-int * buf;
-int * decalage;
-float * resultat;
+void get_var_user_f_sp_ (int *buf,int *decalage,float *resultat)
 {
  float *res;
    res= ((float*) (sav_buf[*buf]) )+ (*decalage -1);
@@ -112,95 +105,72 @@ float * resultat;
 }
 
 
-void get_var_user_f_sp(buf,decalage,resultat)
-int * buf, * decalage;float * resultat;
+void get_var_user_f_sp(int *buf,int *decalage,float *resultat)
 {get_var_user_f_sp_(buf,decalage,resultat);}
 
 
-void _FCALL GET_VAR_USER_F_SP(buf,decalage,resultat)
-int * buf, * decalage;float * resultat;
+void _FCALL GET_VAR_USER_F_SP(int *buf,int *decalage,float *resultat)
 {get_var_user_f_sp_(buf,decalage,resultat);}
 
 
-void get_var_user_f_sp__(buf,decalage,resultat)
-int * buf, * decalage;float * resultat;
+void get_var_user_f_sp__(int *buf,int *decalage,float *resultat)
 {get_var_user_f_sp_(buf,decalage,resultat);}
 
 
-void set_var_user_f_ (buf,decalage,variable)
-int * buf, * decalage;
-double * variable;
+void set_var_user_f_ (int *buf,int *decalage,double *variable)
 {
  double *res;
    res= ((double*)(sav_buf[*buf])) +(*decalage -1);
    *res = *variable;
 }
 
-void set_var_user_f(buf,decalage,variable)
-int * buf, * decalage;double * variable;
+void set_var_user_f(int *buf,int *decalage,double *variable)
 {set_var_user_f_(buf,decalage,variable);}
 
-void _FCALL SET_VAR_USER_F(buf,decalage,variable)
-int * buf, * decalage;double * variable;
-{set_var_user_f_(buf,decalage,variable);}
-
-
-void set_var_user_f__(buf,decalage,variable)
-int * buf, * decalage;double * variable;
+void _FCALL SET_VAR_USER_F(int *buf,int *decalage,double *variable)
 {set_var_user_f_(buf,decalage,variable);}
 
 
-void set_var_user_f_sp_ (buf,decalage,variable)
-int * buf;
-int * decalage;
-float * variable;
+void set_var_user_f__(int *buf,int *decalage,double *variable)
+{set_var_user_f_(buf,decalage,variable);}
+
+
+void set_var_user_f_sp_ (int *buf,int *decalage,float *variable)
 {
  float *res;
    res= ((float*) (sav_buf[*buf]))+ (*decalage -1);
    *res = *variable;
 }
 
-void set_var_user_f_sp(buf,decalage,variable)
-int * buf, * decalage;float * variable;
+void set_var_user_f_sp(int *buf,int *decalage,float *variable)
 {set_var_user_f_sp_(buf,decalage,variable);}
 
-void _FCALL SET_VAR_USER_F_SP(buf,decalage,variable)
-int * buf, * decalage;float * variable;
-{set_var_user_f_sp_(buf,decalage,variable);}
-
-
-void set_var_user_f_sp__(buf,decalage,variable)
-int * buf, * decalage;float * variable;
+void _FCALL SET_VAR_USER_F_SP(int *buf,int *decalage,float *variable)
 {set_var_user_f_sp_(buf,decalage,variable);}
 
 
-void get_var_user_i_(buf,decalage,resultat)
-int * buf;
-int * decalage;
-int * resultat;
+void set_var_user_f_sp__(int *buf,int *decalage,float *variable)
+{set_var_user_f_sp_(buf,decalage,variable);}
+
+
+void get_var_user_i_(int *buf,int *decalage,int *resultat)
 {
  int *res;
  res= (int*) (sav_buf[*buf]+ (*decalage) -1);
  *resultat = *res;
 }
 
-void get_var_user_i(buf,decalage,resultat)
-int * buf, * decalage,* resultat;
+void get_var_user_i(int *buf,int *decalage,int *resultat)
 {get_var_user_i_(buf,decalage,resultat);}
 
-void _FCALL GET_VAR_USER_I(buf,decalage,resultat)
-int * buf, * decalage, * resultat;
+void _FCALL GET_VAR_USER_I(int *buf,int *decalage,int *resultat)
 {get_var_user_i_(buf,decalage,resultat);}
-/*ow41n4+++*/
-void get_var_user_i__(buf,decalage,resultat)
-int * buf, * decalage,* resultat;
+
+void get_var_user_i__(int *buf,int *decalage,int *resultat)
 {get_var_user_i_(buf,decalage,resultat);}
 
 
-void set_var_user_i_(buf,decalage,variable)
-int * buf;
-int * decalage;
-int * variable;
+void set_var_user_i_(int *buf,int *decalage,int *variable)
 {
  int *res;
  res= (int*) (sav_buf[*buf]+ (*decalage) -1);
@@ -208,24 +178,19 @@ int * variable;
 }
 
 
-void set_var_user_i(buf,decalage,variable)
-int * buf, * decalage,* variable;
+void set_var_user_i(int *buf,int *decalage,int *variable)
 {set_var_user_i_(buf,decalage,variable);}
 
 
-void _FCALL SET_VAR_USER_I(buf,decalage,variable)
-int * buf, * decalage, * variable;
+void _FCALL SET_VAR_USER_I(int *buf,int *decalage,int *variable)
 {set_var_user_i_(buf,decalage,variable);}
 
 
-void set_var_user_i__(buf,decalage,variable)
-int * buf, * decalage,* variable;
+void set_var_user_i__(int *buf,int *decalage,int *variable)
 {set_var_user_i_(buf,decalage,variable);}
 
 
-void get_array_user_f_(buf,decalage,array,array_lenght)
-int *buf, *decalage, *array_lenght;
-double * array;
+void get_array_user_f_(int *buf,int *decalage,double *array,int *array_lenght)
 {
  int i;
  double *ar;
@@ -235,24 +200,19 @@ double * array;
 }
 
 
-void get_array_user_f(buf,decalage,array,array_lenght)
-int *buf, *decalage, *array_lenght;double * array;
+void get_array_user_f(int *buf,int *decalage,double *array,int *array_lenght)
 {get_array_user_f_(buf,decalage,array,array_lenght);}
 
 
-void _FCALL GET_ARRAY_USER_F(buf,decalage,array,array_lenght)
-int *buf, *decalage, *array_lenght;double * array;
+void _FCALL GET_ARRAY_USER_F(int *buf,int *decalage,double *array,int *array_lenght)
 {get_array_user_f_(buf,decalage,array,array_lenght);}
 
 
-void get_array_user_f__(buf,decalage,array,array_lenght)
-int *buf, *decalage, *array_lenght;double * array;
+void get_array_user_f__(int *buf,int *decalage,double *array,int *array_lenght)
 {get_array_user_f_(buf,decalage,array,array_lenght);}
 
 
-void set_array_user_f_(buf,decalage,array,array_lenght)
-int *buf, *decalage, *array_lenght;
-double * array;
+void set_array_user_f_(int *buf,int *decalage,double *array,int *array_lenght)
 {
  int i;
  double *ar;
@@ -262,24 +222,19 @@ double * array;
 }
 
 
-void set_array_user_f(buf,decalage,array,array_lenght)
-int *buf, *decalage, *array_lenght;double * array;
+void set_array_user_f(int *buf,int *decalage,double *array,int *array_lenght)
 {set_array_user_f_(buf,decalage,array,array_lenght);}
 
 
-void _FCALL SET_ARRAY_USER_F(buf,decalage,array,array_lenght)
-int *buf, *decalage, *array_lenght;double * array;
+void _FCALL SET_ARRAY_USER_F(int *buf,int *decalage,double *array,int *array_lenght)
 {set_array_user_f_(buf,decalage,array,array_lenght);}
 
 
-void set_array_user_f__(buf,decalage,array,array_lenght)
-int *buf, *decalage, *array_lenght;double * array;
+void set_array_user_f__(int *buf,int *decalage,double *array,int *array_lenght)
 {set_array_user_f_(buf,decalage,array,array_lenght);}
 
 
-void get_array_user_f_sp_(buf,decalage,array,array_lenght)
-int *buf, *decalage, *array_lenght;
-float * array;
+void get_array_user_f_sp_(int *buf,int *decalage,float *array,int *array_lenght)
 {
  int i;
  float *ar;
@@ -289,24 +244,19 @@ float * array;
 }
 
 
-void get_array_user_f_sp(buf,decalage,array,array_lenght)
-int *buf, *decalage, *array_lenght;float * array;
+void get_array_user_f_sp(int *buf,int *decalage,float *array,int *array_lenght)
 {get_array_user_f_sp_(buf,decalage,array,array_lenght);}
 
 
-void _FCALL GET_ARRAY_USER_F_SP(buf,decalage,array,array_lenght)
-int *buf, *decalage, *array_lenght;float * array;
+void _FCALL GET_ARRAY_USER_F_SP(int *buf,int *decalage,float *array,int *array_lenght)
 {get_array_user_f_sp_(buf,decalage,array,array_lenght);}
 
 
-void get_array_user_f_sp__(buf,decalage,array,array_lenght)
-int *buf, *decalage, *array_lenght;float * array;
+void get_array_user_f_sp__(int *buf,int *decalage,float *array,int *array_lenght)
 {get_array_user_f_sp_(buf,decalage,array,array_lenght);}
 
 
-void set_array_user_f_sp_(buf,decalage,array,array_lenght)
-int *buf, *decalage, *array_lenght;
-float * array;
+void set_array_user_f_sp_(int *buf,int *decalage,float *array,int *array_lenght)
 {
  int i;
  float *ar;
@@ -316,24 +266,19 @@ float * array;
 }
 
 
-void set_array_user_f_sp(buf,decalage,array,array_lenght)
-int *buf, *decalage, *array_lenght;float * array;
+void set_array_user_f_sp(int *buf,int *decalage,float *array,int *array_lenght)
 {set_array_user_f_sp_(buf,decalage,array,array_lenght);}
 
 
-void _FCALL SET_ARRAY_USER_F_SP(buf,decalage,array,array_lenght)
-int *buf, *decalage, *array_lenght;float * array;
+void _FCALL SET_ARRAY_USER_F_SP(int *buf,int *decalage,float *array,int *array_lenght)
 {set_array_user_f_sp_(buf,decalage,array,array_lenght);}
 
 
-void set_array_user_f_sp__(buf,decalage,array,array_lenght)
-int *buf, *decalage, *array_lenght;float * array;
+void set_array_user_f_sp__(int *buf,int *decalage,float *array,int *array_lenght)
 {set_array_user_f_sp_(buf,decalage,array,array_lenght);}
 
 
-void get_array_user_i_(buf,decalage,array,array_lenght)
-int *buf, *decalage, *array_lenght;
-int * array;
+void get_array_user_i_(int *buf,int *decalage,int *array,int *array_lenght)
 {
  int i;
  int *ar;
@@ -343,25 +288,20 @@ int * array;
 }
 
 
-void get_array_user_i(buf,decalage,array,array_lenght)
-int *buf, *decalage, *array_lenght;int * array;
+void get_array_user_i(int *buf,int *decalage,int *array,int *array_lenght)
 {get_array_user_i_(buf,decalage,array,array_lenght);}
 
 
-void _FCALL GET_ARRAY_USER_I(buf,decalage,array,array_lenght)
-int *buf, *decalage, *array_lenght;int * array;
+void _FCALL GET_ARRAY_USER_I(int *buf,int *decalage,int *array,int *array_lenght)
 {get_array_user_i_(buf,decalage,array,array_lenght);}
 
 
-void get_array_user_i__(buf,decalage,array,array_lenght)
-int *buf, *decalage, *array_lenght;int * array;
+void get_array_user_i__(int *buf,int *decalage,int *array,int *array_lenght)
 {get_array_user_i_(buf,decalage,array,array_lenght);}
 
 
 
-void set_array_user_i_(buf,decalage,array,array_lenght)
-int *buf, *decalage, *array_lenght;
-int * array;
+void set_array_user_i_(int *buf,int *decalage,int *array,int *array_lenght)
 {
  int i;
  int *ar;
@@ -371,42 +311,33 @@ int * array;
 }
 
 
-void set_array_user_i(buf,decalage,array,array_lenght)
-int *buf, *decalage, *array_lenght;int * array;
+void set_array_user_i(int *buf,int *decalage,int *array,int *array_lenght)
 {set_array_user_i_(buf,decalage,array,array_lenght);}
 
 
-void _FCALL SET_ARRAY_USER_I(buf,decalage,array,array_lenght)
-int *buf, *decalage, *array_lenght;int * array;
+void _FCALL SET_ARRAY_USER_I(int *buf,int *decalage,int *array,int *array_lenght)
 {set_array_user_i_(buf,decalage,array,array_lenght);}
 
 
-void set_array_user_i__(buf,decalage,array,array_lenght)
-int *buf, *decalage, *array_lenght;int * array;
+void set_array_user_i__(int *buf,int *decalage,int *array,int *array_lenght)
 {set_array_user_i_(buf,decalage,array,array_lenght);}
 
 
-void get_user_pointer_(buf,decalage,ptr)
-int *buf;
-int *decalage;
-int **ptr;
+void get_user_pointer_(int *buf,int *decalage,int **ptr)
 {
  *ptr = sav_buf[*buf]+ (*decalage)-1;
 }
 
 
-void get_user_pointer(buf,decalage,ptr)
-int *buf,*decalage,**ptr;
+void get_user_pointer(int *buf,int *decalage,int **ptr)
 {get_user_pointer_(buf,decalage,ptr);}
 
 
-void _FCALL GET_USER_POINTER(buf,decalage,ptr)
-int *buf,*decalage,**ptr;
+void _FCALL GET_USER_POINTER(int *buf,int *decalage,int **ptr)
 {get_user_pointer_(buf,decalage,ptr);}
 
 
-void get_user_pointer__(buf,decalage,ptr)
-int *buf,*decalage,**ptr;
+void get_user_pointer__(int *buf,int *decalage,int **ptr)
 {get_user_pointer_(buf,decalage,ptr);}
 
 
@@ -424,27 +355,19 @@ void u_vinter2_(int *jad,int *jpos,int *jlen,int *llt,my_real_c *xx, my_real_c *
 #endif
 }
 
-void u_vinter2(jad,jpos,jlen,llt,xx,dydx,yy)
-int *jad, *jpos, *jlen,*llt;
-my_real_c *xx,*dydx,*yy;
+void u_vinter2(int *jad,int *jpos,int *jlen,int *llt,my_real_c *xx,my_real_c *dydx,my_real_c *yy)
 {u_vinter2_(jad,jpos,jlen,llt,xx,dydx,yy);}
 
 
-void _FCALL U_VINTER2(jad,jpos,jlen,llt,xx,dydx,yy)
-int *jad, *jpos, *jlen,*llt;
-my_real_c *xx,*dydx,*yy;
+void _FCALL U_VINTER2(int *jad,int *jpos,int *jlen,int *llt,my_real_c *xx,my_real_c *dydx,my_real_c *yy)
 {u_vinter2_(jad,jpos,jlen,llt,xx,dydx,yy);}
 
 
-void u_vinter2__(jad,jpos,jlen,llt,xx,dydx,yy)
-int *jad, *jpos, *jlen,*llt;
-my_real_c *xx,*dydx,*yy;
+void u_vinter2__(int *jad,int *jpos,int *jlen,int *llt,my_real_c *xx,my_real_c *dydx,my_real_c *yy)
 {u_vinter2_(jad,jpos,jlen,llt,xx,dydx,yy);}
 
 
-void u_vinter2dp_(jad,jpos,jlen,llt,xx,dydx,yy)
-int *jad, *jpos, *jlen,*llt;
-double *xx,*dydx,*yy;
+void u_vinter2dp_(int *jad,int *jpos,int *jlen,int *llt,my_real_c *xx,my_real_c *dydx,my_real_c *yy)
 {
  double *tf;
  tf= (double*)sav_buf[6];
@@ -457,26 +380,19 @@ double *xx,*dydx,*yy;
 #endif
 }
 
-void u_vinter2dp(jad,jpos,jlen,llt,xx,dydx,yy)
-int *jad, *jpos, *jlen,*llt;
-my_real_c *xx,*dydx,*yy;
+void u_vinter2dp(int *jad,int *jpos,int *jlen,int *llt,my_real_c *xx,my_real_c *dydx,my_real_c *yy)
 {u_vinter2dp_(jad,jpos,jlen,llt,xx,dydx,yy);}
 
 
-void _FCALL U_VINTER2DP(jad,jpos,jlen,llt,xx,dydx,yy)
-int *jad, *jpos, *jlen,*llt;
-my_real_c *xx,*dydx,*yy;
+void _FCALL U_VINTER2DP(int *jad,int *jpos,int *jlen,int *llt,my_real_c *xx,my_real_c *dydx,my_real_c *yy)
 {u_vinter2dp_(jad,jpos,jlen,llt,xx,dydx,yy);}
 
 
-void u_vinter2dp__(jad,jpos,jlen,llt,xx,dydx,yy)
-int *jad, *jpos, *jlen,*llt;
-my_real_c *xx,*dydx,*yy;
+void u_vinter2dp__(int *jad,int *jpos,int *jlen,int *llt,my_real_c *xx,my_real_c *dydx,my_real_c *yy)
 {u_vinter2dp_(jad,jpos,jlen,llt,xx,dydx,yy);}
 
 
-void get_var_user_vfunc_(buf,llt,ifunc,jpos,jad,jlen)
-int *buf,*llt,*ifunc,*jad,*jpos,*jlen;
+void get_var_user_vfunc_(int *buf,int *llt,int *ifunc,int *jpos,int *jad,int *jlen)
 {
  int i,fin;
  int *ar;
@@ -488,29 +404,19 @@ int *buf,*llt,*ifunc,*jad,*jpos,*jlen;
      jad[i]  = 0.5* ar[ifunc[i]-1  ] +1 ;
      jlen[i] = 0.5* ar[ifunc[i]] - jad[i] - jpos[i] ;
     }
-
 }
 
 
-void _FCALL GET_VAR_USER_VFUNC(buf,llt,ifunc,jpos,jad,jlen)
-int *buf,*llt,*ifunc,*jad,*jpos,*jlen;
-{
- get_var_user_vfunc_(buf,llt,ifunc,jpos,jad,jlen);
-}
+void _FCALL GET_VAR_USER_VFUNC(int *buf,int *llt,int *ifunc,int *jpos,int *jad,int *jlen)
+{ get_var_user_vfunc_(buf,llt,ifunc,jpos,jad,jlen);}
 
 
-void get_var_user_vfunc__(buf,llt,ifunc,jpos,jad,jlen)
-int *buf,*llt,*ifunc,*jad,*jpos,*jlen;
-{
- get_var_user_vfunc_(buf,llt,ifunc,jpos,jad,jlen);
-}
+void get_var_user_vfunc__(int *buf,int *llt,int *ifunc,int *jpos,int *jad,int *jlen)
+{ get_var_user_vfunc_(buf,llt,ifunc,jpos,jad,jlen);}
 
 
-void get_var_user_vfunc(buf,llt,ifunc,jpos,jad,jlen)
-int *buf,*llt,*ifunc,*jad,*jpos,*jlen;
-{
- get_var_user_vfunc_(buf,llt,ifunc,jpos,jad,jlen);
-}
+void get_var_user_vfunc(int *buf,int *llt,int *ifunc,int *jpos,int *jad,int *jlen)
+{  get_var_user_vfunc_(buf,llt,ifunc,jpos,jad,jlen); }
 
 
 
