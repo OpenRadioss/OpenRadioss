a715ce64243d84efc5e90b547cce7888b1b6e3a1
laurent-altr <99423513+laurent-altr@users.noreply.github.com>
2025-02-10 17:10:47 +0100

    node splitting: update
    Compatibility with /INTER/TYPE7 interface

--- common_source/tools/container/umap_mod.F90
+++ common_source/tools/container/umap_mod.F90
@@ -20,67 +20,67 @@
 !Copyright>        As an alternative to this open-source version, Altair also offers Altair Radioss
 !Copyright>        software under a commercial license.  Contact Altair to discuss further if the
 !Copyright>        commercial version may interest you: https://www.altair.com/radioss/.
-module umap_mod
-   use iso_c_binding
-   implicit none
+      module umap_mod
+        use iso_c_binding
+        implicit none
 
-   interface
-      function create_umap() bind(C, name="cpp_create_umap")
-         use iso_c_binding
-         type(C_PTR) :: create_umap
-      end function create_umap
+        interface
+          function create_umap() bind(C, name="cpp_create_umap")
+            use iso_c_binding
+            type(C_PTR) :: create_umap
+          end function create_umap
 
-      subroutine free_umap(umap_ptr) bind(C, name="cpp_free_umap")
-         use iso_c_binding
-         type(C_PTR), value :: umap_ptr
-      end subroutine free_umap
+          subroutine free_umap(umap_ptr) bind(C, name="cpp_free_umap")
+            use iso_c_binding
+            type(C_PTR), value :: umap_ptr
+          end subroutine free_umap
 
-      subroutine add_entry_umap(umap_ptr, key, value) bind(C, name="cpp_add_entry_umap")
-         use iso_c_binding
-         type(C_PTR), value :: umap_ptr
-         integer(C_INT), value :: key
-         integer(C_INT), value :: value
-      end subroutine add_entry_umap
+          subroutine add_entry_umap(umap_ptr, key, value) bind(C, name="cpp_add_entry_umap")
+            use iso_c_binding
+            type(C_PTR), value :: umap_ptr
+            integer(C_INT), value :: key
+            integer(C_INT), value :: value
+          end subroutine add_entry_umap
 
-      function get_value_umap(umap_ptr, key, default_value) result(val) bind(C, name="cpp_get_value_umap")
-         use iso_c_binding
-         type(C_PTR), value :: umap_ptr
-         integer(C_INT), value :: key
-         integer(C_INT), value :: default_value
-         integer(C_INT) :: val
-      end function get_value_umap
+          function get_value_umap(umap_ptr, key, default_value) result(val) bind(C, name="cpp_get_value_umap")
+            use iso_c_binding
+            type(C_PTR), value :: umap_ptr
+            integer(C_INT), value :: key
+            integer(C_INT), value :: default_value
+            integer(C_INT) :: val
+          end function get_value_umap
 
-      subroutine reserve_umap(umap_ptr, n) bind(C, name="cpp_reserve_umap")
-         use iso_c_binding
-         type(C_PTR), value :: umap_ptr
-         ! size_t is typically mapped to C_SIZE_T
-         integer(C_SIZE_T), value :: n
-      end subroutine reserve_umap
-   end interface
+          subroutine reserve_umap(umap_ptr, n) bind(C, name="cpp_reserve_umap")
+            use iso_c_binding
+            type(C_PTR), value :: umap_ptr
+            ! size_t is typically mapped to C_SIZE_T
+            integer(C_SIZE_T), value :: n
+          end subroutine reserve_umap
+        end interface
 
-contains
+      contains
 
-   subroutine add_entry(m, key, value)
-      type(C_PTR), intent(in) :: m
-      integer, intent(in)     :: key, value
+        subroutine add_entry(m, key, value)
+          type(C_PTR), intent(in) :: m
+          integer, intent(in)     :: key, value
 !$OMP CRITICAL
-      call add_entry_umap(m, int(key, C_INT), int(value, C_INT))
+          call add_entry_umap(m, int(key, C_INT), int(value, C_INT))
 !$OMP END CRITICAL
-   end subroutine add_entry
+        end subroutine add_entry
 
-   function get_value(m, key, default_value) result(val)
-      type(C_PTR), intent(in) :: m
-      integer, intent(in)     :: key, default_value
-      integer                 :: val
-      val = get_value_umap(m, int(key, C_INT), int(default_value, C_INT))
-   end function get_value
+        function get_value(m, key, default_value) result(val)
+          type(C_PTR), intent(in) :: m
+          integer, intent(in)     :: key, default_value
+          integer                 :: val
+          val = get_value_umap(m, int(key, C_INT), int(default_value, C_INT))
+        end function get_value
 
-   subroutine reserve_capacity(m, n)
-      type(C_PTR), intent(in) :: m
-      integer, intent(in)     :: n
+        subroutine reserve_capacity(m, n)
+          type(C_PTR), intent(in) :: m
+          integer, intent(in)     :: n
 !$OMP CRITICAL
-      call reserve_umap(m, int(n, C_SIZE_T))
+          call reserve_umap(m, int(n, C_SIZE_T))
 !$OMP END CRITICAL
-   end subroutine reserve_capacity
+        end subroutine reserve_capacity
 
-end module umap_mod
+      end module umap_mod
