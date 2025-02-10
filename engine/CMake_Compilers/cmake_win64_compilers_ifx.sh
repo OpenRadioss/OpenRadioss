c818776c0489840d8b013365b8357bf66e1b9482
OlivierWienholtz <98882516+OlivierWienholtz@users.noreply.github.com>
2025-02-10 16:59:10 +0100

    OpenRadioss Build on Windows: 1/ Upgrade build configurations for Windows: rename _win64 to _win64_ifort configuration rename _win64_ifx to _win64 It is requested to use IntelOneAPI 2025.0.1 or higher to build this executable.
    2/ Modify Howto.md :
    Upgrade compiler for Windows
    Remove "Building environment using cygwin" and "Build OpenRadioss with cygwin" sections.
    
    3/ on new clones : engine/extlib might be ignored.

--- engine/CMake_Compilers/cmake_win64_compilers_ifx.sh
+++
@@ -1,5 +0,0 @@
-# Fixed compiler settings for Win64 Intel Compilers
-Fortran_comp=ifx.exe
-C_comp=icx.exe
-CXX_comp=icx.exe
-CPP_comp=icx.exe
--- starter/CMake_Compilers/cmake_win64_ifx_compilers.sh
+++
@@ -1,4 +0,0 @@
-Fortran_comp=ifx.exe
-C_comp=icx.exe
-CXX_comp=icx.exe
-CPP_comp=icx.exe
