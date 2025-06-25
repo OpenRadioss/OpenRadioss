OpenRadioss/OpenRadioss

commit e8b1eda1e3b6262493f66c35498507f0621486b1

Fix unitialized variable in mulawc.F90

engine/source/materials/mat_share/mulawc.F90
@@ -523,6 +523,7 @@
           mom(jft:jlt,2)  = zero
           mom(jft:jlt,3)  = zero
           yld(jft:jlt)    = zero
+          sigy(jft:jlt)   = zero
           if(flag_zcfac .and. mtn /= 22) zcfac(jft:jlt,1)= zero
           zcfac(jft:jlt,2)= zero
           if(flag_zcfac) zcfac(jft:jlt,2)= one
@@ -553,7 +554,6 @@
           do i=jft,jlt
             dt_inv(i) = dt1c(i)/max(dt1c(i)**2,em20)
           enddo
-          if (flag_law25 .or. flag_law2) sigy(jft:jlt) = zero
 !-----------------------------------------------------------
 !     equivalent strain rate (au sens energetique)
 !-----------------------------------------------------------

commit a205b184be9f3856c5f4f2d18e00ead9cead94f8

regression of QEPH in e8b1eda1e3b6262493f66c35498507f0621486b1

engine/source/materials/mat_share/mulawc.F90
@@ -536,7 +536,7 @@
           mom(jft:jlt,3)  = zero
           yld(jft:jlt)    = zero
           seq0(jft:jlt)   = zero
-          sigy(jft:jlt)   = zero
+          if (flag_law2 .or. flag_law25 .or. (.not.flag_zcfac)) sigy(jft:jlt)   = zero
           if(flag_zcfac .and. mtn /= 22) zcfac(jft:jlt,1)= zero
           zcfac(jft:jlt,2)= zero
           if(flag_zcfac) zcfac(jft:jlt,2)= one