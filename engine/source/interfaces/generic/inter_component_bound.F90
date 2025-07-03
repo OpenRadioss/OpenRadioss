OpenRadioss/OpenRadioss

commit 3ae690cb8d9592c44ed6f3c685d9bc7f1752458c

Fix an omp issue introduced by 1e7919be325e93f462c20fcb102e0000090a8029

engine/source/interfaces/generic/inter_component_bound.F90
@@ -69,6 +69,7 @@
 ! ----------------------------------------------------------------------------------------------------------------------
         integer :: i,ncomp
         integer :: node_id
+        my_real, dimension(6) :: local_bound
 ! ----------------------------------------------------------------------------------------------------------------------
 !                                                   External functions
 ! ----------------------------------------------------------------------------------------------------------------------
@@ -76,41 +77,78 @@
 ! ----------------------------------------------------------------------------------------------------------------------
 !                                                   Body
 ! ----------------------------------------------------------------------------------------------------------------------
+        
+!$omp single          
         do ncomp=1,component%s_comp_nb
           component%s_list(ncomp)%bound(1:3) = -ep30
           component%s_list(ncomp)%bound(4:6) = ep30
-          do i=1,component%s_list(ncomp)%node_nb
-            node_id = component%s_list(ncomp)%node(i)
-            component%s_list(ncomp)%bound(1) = max(component%s_list(ncomp)%bound(1),x(1,node_id))
-            component%s_list(ncomp)%bound(2) = max(component%s_list(ncomp)%bound(2),x(2,node_id))
-            component%s_list(ncomp)%bound(3) = max(component%s_list(ncomp)%bound(3),x(3,node_id))
-            component%s_list(ncomp)%bound(4) = min(component%s_list(ncomp)%bound(4),x(1,node_id))
-            component%s_list(ncomp)%bound(5) = min(component%s_list(ncomp)%bound(5),x(2,node_id))
-            component%s_list(ncomp)%bound(6) = min(component%s_list(ncomp)%bound(6),x(3,node_id))            
-          enddo
         enddo
-
         do ncomp=1,component%m_comp_nb
           component%m_list(ncomp)%bound(1:3) = -ep30
           component%m_list(ncomp)%bound(4:6) = ep30
+        enddo
+!$omp end single     
+                
+        do ncomp=1,component%s_comp_nb
+          local_bound(1:3) = -ep30
+          local_bound(4:6) = ep30
+!$omp parallel do            
+          do i=1,component%s_list(ncomp)%node_nb
+            node_id = component%s_list(ncomp)%node(i)
+            local_bound(1) = max(local_bound(1),x(1,node_id))
+            local_bound(2) = max(local_bound(2),x(2,node_id))
+            local_bound(3) = max(local_bound(3),x(3,node_id))
+            local_bound(4) = min(local_bound(4),x(1,node_id))
+            local_bound(5) = min(local_bound(5),x(2,node_id))
+            local_bound(6) = min(local_bound(6),x(3,node_id))
+          enddo
+!$omp end parallel do
+
+!$omp critical
+          component%s_list(ncomp)%bound(1) = max(component%s_list(ncomp)%bound(1),local_bound(1))
+          component%s_list(ncomp)%bound(2) = max(component%s_list(ncomp)%bound(2),local_bound(2))
+          component%s_list(ncomp)%bound(3) = max(component%s_list(ncomp)%bound(3),local_bound(3))
+          component%s_list(ncomp)%bound(4) = min(component%s_list(ncomp)%bound(4),local_bound(4))
+          component%s_list(ncomp)%bound(5) = min(component%s_list(ncomp)%bound(5),local_bound(5))
+          component%s_list(ncomp)%bound(6) = min(component%s_list(ncomp)%bound(6),local_bound(6))
+!$omp end critical
+        enddo
+
+        do ncomp=1,component%m_comp_nb
+          local_bound(1:3) = -ep30
+          local_bound(4:6) = ep30
+!$omp parallel do           
           do i=1,component%m_list(ncomp)%node_nb
             node_id = component%m_list(ncomp)%node(i)
-            component%m_list(ncomp)%bound(1) = max(component%m_list(ncomp)%bound(1),x(1,node_id))
-            component%m_list(ncomp)%bound(2) = max(component%m_list(ncomp)%bound(2),x(2,node_id))
-            component%m_list(ncomp)%bound(3) = max(component%m_list(ncomp)%bound(3),x(3,node_id))
-            component%m_list(ncomp)%bound(4) = min(component%m_list(ncomp)%bound(4),x(1,node_id))
-            component%m_list(ncomp)%bound(5) = min(component%m_list(ncomp)%bound(5),x(2,node_id))
-            component%m_list(ncomp)%bound(6) = min(component%m_list(ncomp)%bound(6),x(3,node_id))            
+            local_bound(1) = max(local_bound(1),x(1,node_id))
+            local_bound(2) = max(local_bound(2),x(2,node_id))
+            local_bound(3) = max(local_bound(3),x(3,node_id))
+            local_bound(4) = min(local_bound(4),x(1,node_id))
+            local_bound(5) = min(local_bound(5),x(2,node_id))
+            local_bound(6) = min(local_bound(6),x(3,node_id))
           enddo
+!$omp end parallel do
+          
+!$omp critical          
+            component%m_list(ncomp)%bound(1) = max(component%m_list(ncomp)%bound(1),local_bound(1))
+            component%m_list(ncomp)%bound(2) = max(component%m_list(ncomp)%bound(2),local_bound(2))
+            component%m_list(ncomp)%bound(3) = max(component%m_list(ncomp)%bound(3),local_bound(3))
+            component%m_list(ncomp)%bound(4) = min(component%m_list(ncomp)%bound(4),local_bound(4))
+            component%m_list(ncomp)%bound(5) = min(component%m_list(ncomp)%bound(5),local_bound(5))
+            component%m_list(ncomp)%bound(6) = min(component%m_list(ncomp)%bound(6),local_bound(6))            
+!$omp end critical
+        enddo    
 
+!$omp single          
+        do ncomp=1,component%m_comp_nb
           component%m_list(ncomp)%bound(1) = component%m_list(ncomp)%bound(1) + tzinf + curv
           component%m_list(ncomp)%bound(2) = component%m_list(ncomp)%bound(2) + tzinf + curv
           component%m_list(ncomp)%bound(3) = component%m_list(ncomp)%bound(3) + tzinf + curv
           component%m_list(ncomp)%bound(4) = component%m_list(ncomp)%bound(4) - tzinf - curv
           component%m_list(ncomp)%bound(5) = component%m_list(ncomp)%bound(5) - tzinf - curv
           component%m_list(ncomp)%bound(6) = component%m_list(ncomp)%bound(6) - tzinf - curv
-        enddo        
-
+        enddo
+!$omp end single     
 
 
 ! ----------------------------------------------------------------------------------------------------------------------

commit 82f522511e630e06bc6355f592050b4dfdb9d630

Fix an OpenMP issue introduced by 3ae690cb8d9592c44ed6f3c685d9bc7f1752458c

engine/source/interfaces/generic/inter_component_bound.F90
@@ -78,7 +78,7 @@
 !                                                   Body
 ! ----------------------------------------------------------------------------------------------------------------------
         
-!$omp single          
+        !$omp single          
         do ncomp=1,component%s_comp_nb
           component%s_list(ncomp)%bound(1:3) = -ep30
           component%s_list(ncomp)%bound(4:6) = ep30
@@ -87,12 +87,12 @@
           component%m_list(ncomp)%bound(1:3) = -ep30
           component%m_list(ncomp)%bound(4:6) = ep30
         enddo
-!$omp end single     
+        !$omp end single     
                 
         do ncomp=1,component%s_comp_nb
           local_bound(1:3) = -ep30
           local_bound(4:6) = ep30
-!$omp parallel do            
+          !$omp do schedule(dynamic)
           do i=1,component%s_list(ncomp)%node_nb
             node_id = component%s_list(ncomp)%node(i)
             local_bound(1) = max(local_bound(1),x(1,node_id))
@@ -102,22 +102,22 @@
             local_bound(5) = min(local_bound(5),x(2,node_id))
             local_bound(6) = min(local_bound(6),x(3,node_id))
           enddo
-!$omp end parallel do
+          !$omp end do
 
-!$omp critical
+          !$omp critical
           component%s_list(ncomp)%bound(1) = max(component%s_list(ncomp)%bound(1),local_bound(1))
           component%s_list(ncomp)%bound(2) = max(component%s_list(ncomp)%bound(2),local_bound(2))
           component%s_list(ncomp)%bound(3) = max(component%s_list(ncomp)%bound(3),local_bound(3))
           component%s_list(ncomp)%bound(4) = min(component%s_list(ncomp)%bound(4),local_bound(4))
           component%s_list(ncomp)%bound(5) = min(component%s_list(ncomp)%bound(5),local_bound(5))
           component%s_list(ncomp)%bound(6) = min(component%s_list(ncomp)%bound(6),local_bound(6))
-!$omp end critical
+          !$omp end critical
         enddo
 
         do ncomp=1,component%m_comp_nb
           local_bound(1:3) = -ep30
           local_bound(4:6) = ep30
-!$omp parallel do           
+          !$omp do schedule(dynamic)
           do i=1,component%m_list(ncomp)%node_nb
             node_id = component%m_list(ncomp)%node(i)
             local_bound(1) = max(local_bound(1),x(1,node_id))
@@ -127,19 +127,21 @@
             local_bound(5) = min(local_bound(5),x(2,node_id))
             local_bound(6) = min(local_bound(6),x(3,node_id))
           enddo
-!$omp end parallel do
+          !$omp end do
           
-!$omp critical          
-            component%m_list(ncomp)%bound(1) = max(component%m_list(ncomp)%bound(1),local_bound(1))
-            component%m_list(ncomp)%bound(2) = max(component%m_list(ncomp)%bound(2),local_bound(2))
-            component%m_list(ncomp)%bound(3) = max(component%m_list(ncomp)%bound(3),local_bound(3))
-            component%m_list(ncomp)%bound(4) = min(component%m_list(ncomp)%bound(4),local_bound(4))
-            component%m_list(ncomp)%bound(5) = min(component%m_list(ncomp)%bound(5),local_bound(5))
-            component%m_list(ncomp)%bound(6) = min(component%m_list(ncomp)%bound(6),local_bound(6))            
-!$omp end critical
+          !$omp critical          
+          component%m_list(ncomp)%bound(1) = max(component%m_list(ncomp)%bound(1),local_bound(1))
+          component%m_list(ncomp)%bound(2) = max(component%m_list(ncomp)%bound(2),local_bound(2))
+          component%m_list(ncomp)%bound(3) = max(component%m_list(ncomp)%bound(3),local_bound(3))
+          component%m_list(ncomp)%bound(4) = min(component%m_list(ncomp)%bound(4),local_bound(4))
+          component%m_list(ncomp)%bound(5) = min(component%m_list(ncomp)%bound(5),local_bound(5))
+          component%m_list(ncomp)%bound(6) = min(component%m_list(ncomp)%bound(6),local_bound(6))            
+          !$omp end critical
         enddo    
 
-!$omp single          
+        !$omp barrier
+
+        !$omp single          
         do ncomp=1,component%m_comp_nb
           component%m_list(ncomp)%bound(1) = component%m_list(ncomp)%bound(1) + tzinf + curv
           component%m_list(ncomp)%bound(2) = component%m_list(ncomp)%bound(2) + tzinf + curv
@@ -148,9 +150,9 @@
           component%m_list(ncomp)%bound(5) = component%m_list(ncomp)%bound(5) - tzinf - curv
           component%m_list(ncomp)%bound(6) = component%m_list(ncomp)%bound(6) - tzinf - curv
         enddo
-!$omp end single     
+        !$omp end single     
 
-
+        return
 ! ----------------------------------------------------------------------------------------------------------------------
         end subroutine inter_component_bound
       end module inter_component_bound_mod

engine/source/interfaces/generic/inter_prepare_sort.F
@@ -163,11 +163,13 @@ C-----------------------------------------------
             ! FILLS INTER_STRUCT%BOX_LIMIT_MAIN
             CALL INTER_COUNT_NODE_CURV( N,ITASK,IPARI,INTBUF_TAB,
      1                                      X,INTER_STRUCT)
+            CALL MY_BARRIER()
             tzinf = intbuf_tab(n)%variables(tzinf_index)
             curv = inter_struct(n)%curv_max_max
             call inter_component_bound(numnod,tzinf,curv,x,component(n))
           ENDDO
           ! ---------------------
+          CALL MY_BARRIER()
         ENDIF
         ! -----------------------------
 