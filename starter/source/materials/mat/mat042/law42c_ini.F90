328cb0ad641b36b1336a0c9b0f3279bd91116f17
qiangRadioss <105218709+qiangRadioss@users.noreply.github.com>
2025-09-30 09:20:54 +0200

    correction on law42c (shell) to be plane-stress and consistency between Prony input in&out of material

---
+++ starter/source/materials/mat/mat042/law42c_ini.F90
@@ -0,0 +1,55 @@
+!Copyright>        OpenRadioss
+!Copyright>        Copyright (C) 1986-2025 Altair Engineering Inc.
+!Copyright>
+!Copyright>        This program is free software: you can redistribute it and/or modify
+!Copyright>        it under the terms of the GNU Affero General Public License as published by
+!Copyright>        the Free Software Foundation, either version 3 of the License, or
+!Copyright>        (at your option) any later version.
+!Copyright>
+!Copyright>        This program is distributed in the hope that it will be useful,
+!Copyright>        but WITHOUT ANY WARRANTY; without even the implied warranty of
+!Copyright>        MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+!Copyright>        GNU Affero General Public License for more details.
+!Copyright>
+!Copyright>        You should have received a copy of the GNU Affero General Public License
+!Copyright>        along with this program.  If not, see <https://www.gnu.org/licenses/>.
+!Copyright>
+!Copyright>
+!Copyright>        Commercial Alternative: Altair Radioss Software
+!Copyright>
+!Copyright>        As an alternative to this open-source version, Altair also offers Altair Radioss
+!Copyright>        software under a commercial license.  Contact Altair to discuss further if the
+!Copyright>        commercial version may interest you: https://www.altair.com/radioss/.
+      module law42c_ini_mod
+      implicit none
+      contains
+        ! ======================================================================================================================
+        ! \brief initialization UVAR of law42c
+        ! ======================================================================================================================
+!||====================================================================
+        subroutine law42c_ini(nuvar,uvar,nel)
+! ----------------------------------------------------------------------------------------------------------------------
+!                                                        Modules
+! ----------------------------------------------------------------------------------------------------------------------
+          use constant_mod, only : one
+          use precision_mod , only : WP
+! ----------------------------------------------------------------------------------------------------------------------
+!                                                 implicit none
+! ----------------------------------------------------------------------------------------------------------------------
+          implicit none
+! ----------------------------------------------------------------------------------------------------------------------
+!                                                   arguments
+! ----------------------------------------------------------------------------------------------------------------------
+          integer ,intent(in)                              :: nel       !< shell group size
+          integer ,intent(in)                              :: nuvar  !< 1er dimension of uvar
+          real(kind=WP),dimension(nel,nuvar),intent(inout) :: uvar      !< internal work array
+! ----------------------------------------------------------------------------------------------------------------------
+!                                                   local variables
+! ----------------------------------------------------------------------------------------------------------------------
+          integer :: i
+! ======================================================================================================================
+
+          uvar(1:nel,3) = one
+! ----------------------------------------------------------------------------------------------------------------------
+        end subroutine law42c_ini
+      end module law42c_ini_mod
