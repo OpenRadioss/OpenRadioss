a02e2506236b33329128ed7a116edbab2b98eff8
laurent-altr <99423513+laurent-altr@users.noreply.github.com>
2025-12-10 09:36:30 +0100

    Revert "add -debug=chkb option when using Gfortran"
    This reverts commit debb52e77aec9ca6c739efc86045e3a503274d87.

--- common_source/modules/nodal_arrays.F90
+++ common_source/modules/nodal_arrays.F90
@@ -290,17 +290,17 @@
 
 
           if(iparith == 0) then
-            call my_alloc(arrays%A,3,max(numnod,1)*nthreads)
-            call my_alloc(arrays%AR,3,max(1,numnod)*nthreads)
+            call my_alloc(arrays%A,3,numnod*nthreads)
+            call my_alloc(arrays%AR,3,numnod*nthreads)
             call my_alloc(arrays%STIFR,max(numnod*iroddl*nthreads,1))
-            call my_alloc(arrays%VISCN,max(1,numnod)*nthreads)
-            call my_alloc(arrays%STIFN,max(1,numnod)*nthreads)
+            call my_alloc(arrays%VISCN,numnod*nthreads)
+            call my_alloc(arrays%STIFN,numnod*nthreads)
           else
-            call my_alloc(arrays%A,3,max(1,numnod))
-            call my_alloc(arrays%AR,3,max(1,numnod))
-            call my_alloc(arrays%STIFR,max(1,numnod))
-            call my_alloc(arrays%VISCN,max(1,numnod))
-            call my_alloc(arrays%STIFN,max(1,numnod))
+            call my_alloc(arrays%A,3,numnod)
+            call my_alloc(arrays%AR,3,numnod)
+            call my_alloc(arrays%STIFR,numnod)
+            call my_alloc(arrays%VISCN,numnod)
+            call my_alloc(arrays%STIFN,numnod)
           end if
           arrays%numnod = numnod
           ! initialization to 0
