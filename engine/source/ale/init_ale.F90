OpenRadioss/OpenRadioss

commit d3700491e7d04f03e69ab8aef2310c229be07703

ALE : add somes internal timers and first optimizations (gradient reconstruction & array FLUX/QMV)

engine/source/ale/euler2d/eflux2.F
@@ -31,7 +31,7 @@ Copyright>        commercial version may interest you: https://www.altair.com/ra
 !||    element_mod            ../common_source/modules/elements/element_mod.F90
 !||====================================================================
       SUBROUTINE EFLUX2(PM,IXQ,V,X,
-     .                  FLUX,FLU1,FILL,DFILL,VOL,ALE_CONNECT,ALPH)
+     .                  FLUX,FLU1,FILL,DFILL,VOL,ALE_CONNECT,ALPH,s_flux)
 C-----------------------------------------------
 C   M o d u l e s
 C-----------------------------------------------
@@ -55,8 +55,9 @@ C-----------------------------------------------
 C-----------------------------------------------
 C   D u m m y   A r g u m e n t s
 C-----------------------------------------------
+      integer, intent(in) :: s_flux !< first dimension of FLUX array
       INTEGER IXQ(NIXQ,NUMELQ)
-      my_real PM(NPROPM,NUMMAT), V(3,NUMNOD), X(3,NUMNOD), FLUX(4,*), FLU1(*), FILL(*), DFILL(*), VOL(*), ALPH(*)
+      my_real PM(NPROPM,NUMMAT), V(3,NUMNOD), X(3,NUMNOD), FLUX(s_flux,4), FLU1(*), FILL(*), DFILL(*), VOL(*), ALPH(*)
       TYPE(t_ale_connectivity), INTENT(IN) :: ALE_CONNECT
 C-----------------------------------------------
 C   L o c a l   V a r i a b l e s
@@ -70,7 +71,7 @@ C-----------------------------------------------
      .   VY1(MVSIZ), VY2(MVSIZ), VY3(MVSIZ), VY4(MVSIZ), VZ1(MVSIZ), VZ2(MVSIZ), VZ3(MVSIZ),
      .   VZ4(MVSIZ), VDY1(MVSIZ), VDY2(MVSIZ),
      .   VDY3(MVSIZ), VDY4(MVSIZ), VDZ1(MVSIZ), VDZ2(MVSIZ), VDZ3(MVSIZ), VDZ4(MVSIZ), UPW(MVSIZ),
-     .   REDUC,UPWL(4,MVSIZ)
+     .   REDUC,UPWL(MVSIZ,4)
 C-----------------------------------------------
 C   S o u r c e   L i n e s 
 C-----------------------------------------------     
@@ -162,10 +163,10 @@ C     SPECIFIC CASE LAW51
 C-----------------------------------------------
       IF(NINT(PM(19,MAT(1))) == 51)THEN
         DO I=LFT,LLT
-          FLUX(1,I)=FLUX1(I)
-          FLUX(2,I)=FLUX2(I)
-          FLUX(3,I)=FLUX3(I)
-          FLUX(4,I)=FLUX4(I)
+          FLUX(I+NFT,1)=FLUX1(I)
+          FLUX(I+NFT,2)=FLUX2(I)
+          FLUX(I+NFT,3)=FLUX3(I)
+          FLUX(I+NFT,4)=FLUX4(I)
         ENDDO
         RETURN
       ENDIF
@@ -174,7 +175,7 @@ C     UPWIND
 C-----------------------------------------------
       DO J=1,4
         DO I=LFT,LLT
-          UPWL(J,I) = PM(16,MAT(I))
+          UPWL(I,J) = PM(16,MAT(I))
         ENDDO
       ENDDO
       
@@ -216,17 +217,17 @@ C-----------------------------------------------
         ENDDO
         CALL AMULF2(FILL,DFILL,FLUX,FLU1,VOL,ALE_CONNECT,ALPH,
      .     FLUX1, FLUX2, FLUX3, FLUX4, UPW, 
-     .     NC1, NC2, NC3, NC4)
+     .     NC1, NC2, NC3, NC4,s_flux)
       ELSE
        DO I=LFT,LLT
-         FLUX(1,I)= FLUX1(I) - UPWL(1,I) * ABS(FLUX1(I))
-         FLUX(2,I)= FLUX2(I) - UPWL(2,I) * ABS(FLUX2(I))
-         FLUX(3,I)= FLUX3(I) - UPWL(3,I) * ABS(FLUX3(I))
-         FLUX(4,I)= FLUX4(I) - UPWL(4,I) * ABS(FLUX4(I))
-         FLU1(I)  = FLUX1(I) + UPWL(1,I) * ABS(FLUX1(I))
-     .            + FLUX2(I) + UPWL(2,I) * ABS(FLUX2(I))
-     .            + FLUX3(I) + UPWL(3,I) * ABS(FLUX3(I))
-     .            + FLUX4(I) + UPWL(4,I) * ABS(FLUX4(I))
+         FLUX(I+NFT,1)= FLUX1(I) - UPWL(I,1) * ABS(FLUX1(I))
+         FLUX(I+NFT,2)= FLUX2(I) - UPWL(I,2) * ABS(FLUX2(I))
+         FLUX(I+NFT,3)= FLUX3(I) - UPWL(I,3) * ABS(FLUX3(I))
+         FLUX(I+NFT,4)= FLUX4(I) - UPWL(I,4) * ABS(FLUX4(I))
+         FLU1(I)  = FLUX1(I) + UPWL(I,1) * ABS(FLUX1(I))
+     .            + FLUX2(I) + UPWL(I,2) * ABS(FLUX2(I))
+     .            + FLUX3(I) + UPWL(I,3) * ABS(FLUX3(I))
+     .            + FLUX4(I) + UPWL(I,4) * ABS(FLUX4(I))
        ENDDO
       ENDIF
 

engine/source/ale/init_ale.F90
@@ -1,0 +1,89 @@
+!Copyright>        OpenRadioss
+!Copyright>        Copyright (C) 1986-2025 Altair Engineering Inc.
+!Copyright>
+!Copyright>        This program is free software: you can redistribute it and/or modify
+!Copyright>        it under the terms of the GNU Affero General Public License as published by
+!Copyright>        the Free Software Foundation, either version 3 of the License, or
+!Copyright>        (at your option) any later version.
+!Copyright>
+!Copyright>        This program is distributed in the hope that it will be useful,
+!Copyright>        but WITHOUT ANY WARRANTY; without even the implied warranty of
+!Copyright>        MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+!Copyright>        GNU Affero General Public License for more details.
+!Copyright>
+!Copyright>        You should have received a copy of the GNU Affero General Public License
+!Copyright>        along with this program.  If not, see <https://www.gnu.org/licenses/>.
+!Copyright>
+!Copyright>
+!Copyright>        Commercial Alternative: Altair Radioss Software
+!Copyright>
+!Copyright>        As an alternative to this open-source version, Altair also offers Altair Radioss
+!Copyright>        software under a commercial license.  Contact Altair to discuss further if the
+!Copyright>        commercial version may interest you: https://www.altair.com/radioss/.
+      module init_ale_mod
+        implicit none
+      contains
+! ======================================================================================================================
+!                                                   procedures
+! ======================================================================================================================
+!! \brief
+!! \details
+        subroutine init_ale(global_active_ale_element,n2d,numels,numelq,nmult,iale,ieuler,trimat,itherm,ale)
+! ----------------------------------------------------------------------------------------------------------------------
+!                                                   Modules
+! ----------------------------------------------------------------------------------------------------------------------
+          use ale_mod , only : ale_
+! ----------------------------------------------------------------------------------------------------------------------
+!                                                   Implicit none
+! ----------------------------------------------------------------------------------------------------------------------
+          implicit none
+! ----------------------------------------------------------------------------------------------------------------------
+!                                                   Included files
+! ----------------------------------------------------------------------------------------------------------------------
+! ----------------------------------------------------------------------------------------------------------------------
+!                                                   Arguments
+! ----------------------------------------------------------------------------------------------------------------------
+          logical, intent(in) :: global_active_ale_element !< global flag for ALE element activation
+          integer, intent(in) :: n2d !< 0: 3D, 1: 2D
+          integer, intent(in) :: numels !< number of solid elements
+          integer, intent(in) :: numelq !< number of quad elements
+          integer, intent(in) :: nmult !< number of ALE materials (2d case)
+          integer, intent(in) :: iale !< ALE activated flag
+          integer, intent(in) :: ieuler !< Eulerian activated flag
+          integer, intent(in) :: trimat !< number of sub-materials
+          integer, intent(in) :: itherm !< thermal activated flag
+
+          type(ale_), intent(inout) :: ale !< ALE data structure
+
+! ----------------------------------------------------------------------------------------------------------------------
+!                                                   Local variables
+! ----------------------------------------------------------------------------------------------------------------------
+! ----------------------------------------------------------------------------------------------------------------------
+!                                                   External functions
+! ----------------------------------------------------------------------------------------------------------------------
+! [ external functions must be kept to minimum ]
+! ----------------------------------------------------------------------------------------------------------------------
+!                                                   Body
+! ----------------------------------------------------------------------------------------------------------------------
+          if(n2d==0) then
+            ale%global%nv46 = 6
+          else
+            ale%global%nv46 = 4
+          end if
+          ale%global%s_flux = 0
+          ale%global%s_qmv = 0
+          if(iale+ieuler+itherm/=0.and.global_active_ale_element) then
+            if(n2d==0) then
+              ale%global%s_flux = numels+numelq
+            else
+              ale%global%s_flux = max(1,nmult)*4*numelq
+            end if
+
+            ale%global%s_qmv = 1
+            if(trimat>0) ale%global%s_qmv = min(1,trimat)*(numels+numelq)
+          end if
+
+          return
+! ----------------------------------------------------------------------------------------------------------------------
+        end subroutine init_ale
+      end module init_ale_mod

commit e910bed0406cff110844e5776f2a1f6ef7665732

ALE & 2D : fix an issue introduced by d3700491e7d04f03e69ab8aef2310c229be07703. The size %s_flux was wrong

engine/source/ale/euler2d/eflux2.F
@@ -163,10 +163,10 @@ C     SPECIFIC CASE LAW51
 C-----------------------------------------------
       IF(NINT(PM(19,MAT(1))) == 51)THEN
         DO I=LFT,LLT
-          FLUX(I,1)=FLUX1(I)
-          FLUX(I,2)=FLUX2(I)
-          FLUX(I,3)=FLUX3(I)
-          FLUX(I,4)=FLUX4(I)
+          FLUX(I+nft,1)=FLUX1(I)
+          FLUX(I+nft,2)=FLUX2(I)
+          FLUX(I+nft,3)=FLUX3(I)
+          FLUX(I+nft,4)=FLUX4(I)
         ENDDO
         RETURN
       ENDIF
@@ -220,10 +220,10 @@ C-----------------------------------------------
      .     NC1, NC2, NC3, NC4,s_flux)
       ELSE
        DO I=LFT,LLT
-         FLUX(I,1)= FLUX1(I) - UPWL(I,1) * ABS(FLUX1(I))
-         FLUX(I,2)= FLUX2(I) - UPWL(I,2) * ABS(FLUX2(I))
-         FLUX(I,3)= FLUX3(I) - UPWL(I,3) * ABS(FLUX3(I))
-         FLUX(I,4)= FLUX4(I) - UPWL(I,4) * ABS(FLUX4(I))
+         FLUX(nft+I,1)= FLUX1(I) - UPWL(I,1) * ABS(FLUX1(I))
+         FLUX(nft+I,2)= FLUX2(I) - UPWL(I,2) * ABS(FLUX2(I))
+         FLUX(nft+I,3)= FLUX3(I) - UPWL(I,3) * ABS(FLUX3(I))
+         FLUX(nft+I,4)= FLUX4(I) - UPWL(I,4) * ABS(FLUX4(I))
          FLU1(I)  = FLUX1(I) + UPWL(I,1) * ABS(FLUX1(I))
      .            + FLUX2(I) + UPWL(I,2) * ABS(FLUX2(I))
      .            + FLUX3(I) + UPWL(I,3) * ABS(FLUX3(I))

engine/source/ale/init_ale.F90
@@ -88,7 +88,7 @@
             if(n2d==0) then
               ale%global%s_flux = numels+numelq
             else
-              ale%global%s_flux = max(1,nmult)*4*numelq
+              ale%global%s_flux = max(1,nmult)*numelq
             end if
 
             ale%global%s_qmv = 1