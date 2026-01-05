43562a9043518cc2ba81fa1dd1d1407e3fe211f9
laurent-altr <99423513+laurent-altr@users.noreply.github.com>
2026-01-05 09:26:04 +0100

    2026 copyright

--- reader/source/cfgio/MODEL_IO/meci_syntax_infos.cpp
+++ reader/source/cfgio/MODEL_IO/meci_syntax_infos.cpp
@@ -1,5 +1,5 @@
-/*Copyright>    OpenRadioss
-//Copyright>    Copyright (C) 1986-2025 Altair Engineering Inc.
+//Copyright>    OpenRadioss
+//Copyright>    Copyright (C) 1986-2026 Altair Engineering Inc.
 //Copyright>
 //Copyright>    This program is free software: you can redistribute it and/or modify
 //Copyright>    it under the terms of the GNU Affero General Public License as published by
@@ -19,7 +19,7 @@
 //Copyright>
 //Copyright>    As an alternative to this open-source version, Altair also offers Altair Radioss
 //Copyright>    software under a commercial license.  Contact Altair to discuss further if the
-//Copyright>    commercial version may interest you: https://www.altair.com/radioss/.*/
+//Copyright>    commercial version may interest you: https://www.altair.com/radioss/.
 
 #include <string>
 #include <hcioi_utils.h>
