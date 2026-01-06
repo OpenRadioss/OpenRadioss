1723b9d885fae7e39d4aa0d76a0a3e08ea4bbee0
Thierry Schwoertzig <104987175+Schwoertzig@users.noreply.github.com>
2026-01-06 17:29:39 +0100

    /EOS/COMPACTION_TAB : update

--- starter/source/materials/eos/hm_read_eos_compaction_tab.F90
+++ starter/source/materials/eos/hm_read_eos_compaction_tab.F90
@@ -287,7 +287,7 @@
       xvec1(1:1,1) = rhoi
       call table_mat_vinterp(eos_struct%table(1),1,1,vartmp(1,1),xvec1,yy,slope)
       P0 = yy(1)
-      bulk = slope(1)
+      bulk = slope(1)*rhoi  !slope is dP/drho, not dP/dmu
       ! SSP0 = Cunload(rho_0)
       call table_mat_vinterp(eos_struct%table(2),1,1,vartmp(1,2),xvec1,yy,slope)
       SSP0 = yy(1)
