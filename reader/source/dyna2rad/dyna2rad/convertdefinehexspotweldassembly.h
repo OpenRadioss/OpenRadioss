76f8a524cccaa0c7da11091a16568b599a5d20b5
SebastienVilleneuve <100404030+sebastienVilleneuve@users.noreply.github.com>
2026-01-07 14:44:02 +0100

    openreader : correction copyright

--- reader/source/dyna2rad/dyna2rad/convertdefinehexspotweldassembly.h
+++ reader/source/dyna2rad/dyna2rad/convertdefinehexspotweldassembly.h
@@ -1,4 +1,4 @@
-//Copyright>    OpenRadioss
+/*Copyright>    OpenRadioss
 //Copyright>    Copyright (C) 1986-2026 Altair Engineering Inc.
 //Copyright>
 //Copyright>    This program is free software: you can redistribute it and/or modify
@@ -19,7 +19,7 @@
 //Copyright>
 //Copyright>    As an alternative to this open-source version, Altair also offers Altair Radioss
 //Copyright>    software under a commercial license.  Contact Altair to discuss further if the
-//Copyright>    commercial version may interest you: https://www.altair.com/radioss/.
+//Copyright>    commercial version may interest you: https://www.altair.com/radioss/. */
 
 #ifndef SDID2R_CONVERTDEFINEHEXSPOTWELDASSEMBLY_H
 #define SDID2R_CONVERTDEFINEHEXSPOTWELDASSEMBLY_H
