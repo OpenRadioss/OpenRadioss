e3b1b54ffae6ff8377d999eb9c11d5f5d09bb71e
bertrand-altair <103751412+bertrand-altair@users.noreply.github.com>
2026-01-30 17:52:38 +0100

    GUIDED CABLE for 1D elements
    missing file
    
    missing file2
    
    fix xdp

---
+++ starter/source/tools/seatbelts/ini_guided_cable.F90
@@ -0,0 +1,116 @@
+!Copyright>        OpenRadioss
+!Copyright>        Copyright (C) 1986-2025 Altair Engineering Inc.
+!Copyright>
+!Copyright>        This program is free software: you can redistribute it and/or modify
+!Copyright>        it under the terms of the GNU Affero General Public License as published by
+!Copyright>        the Free Software Foundation, either version 3 of the License, or
+!Copyright>        (at your option) any later version.
+!Copyright>
+!Copyright>        This program is distributed in the hope that it will be useful,
+!Copyright>        but WITHOUT ANY WARRANTY; without even the implied warranty of
+!Copyright>        MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+!Copyright>        GNU Affero General Public License for more details.
+!Copyright>
+!Copyright>        You should have received a copy of the GNU Affero General Public License
+!Copyright>        along with this program.  If not, see <https://www.gnu.org/licenses/>.
+!Copyright>
+!Copyright>
+!Copyright>        Commercial Alternative: Altair Radioss Software
+!Copyright>
+!Copyright>        As an alternative to this open-source version, Altair also offers Altair Radioss
+!Copyright>        software under a commercial license.  Contact Altair to discuss further if the
+!Copyright>        commercial version may interest you: https://www.altair.com/radioss/.
+!||====================================================================
+!||    hm_read_bcs_wall_mod   ../starter/source/boundary_conditions/hm_read_bcs_wall.F90
+!||--- called by ------------------------------------------------------
+!||    lectur                 ../starter/source/starter/lectur.F
+!||====================================================================
+      module ini_guided_cable_mod
+        implicit none
+      contains
+! ======================================================================================================================
+!                                                   PROCEDURES
+! ======================================================================================================================
+!! \brief Reader subroutine for option /GUIDE
+!! \details
+!||====================================================================
+!||    hm_read_guide       ../starter/source/tools/seatbelts/hm_read_guide.F90
+!||--- called by ------------------------------------------------------
+!||    lectur                 ../starter/source/starter/lectur.F
+!||--- calls      -----------------------------------------------------
+!||    ancmsg                 ../starter/source/output/message/message.F
+!||    hm_get_floatv          ../starter/source/devtools/hm_reader/hm_get_floatv.F
+!||    hm_get_intv            ../starter/source/devtools/hm_reader/hm_get_intv.F
+!||    hm_option_read_key     ../starter/source/devtools/hm_reader/hm_option_read_key.F
+!||    hm_option_start        ../starter/source/devtools/hm_reader/hm_option_start.F
+!||    nodgrnr5               ../starter/source/starter/freform.F
+!||--- uses       -----------------------------------------------------
+!||    hm_option_read_mod     ../starter/share/modules1/hm_option_read_mod.F
+!||    message_mod            ../starter/share/message_module/message_mod.F
+!||    submodel_mod           ../starter/share/modules1/submodel_mod.F
+!||====================================================================
+        subroutine ini_guided_cable(nspmd)
+! ----------------------------------------------------------------------------------------------------------------------
+!                                                   Modules
+! ----------------------------------------------------------------------------------------------------------------------
+          use message_mod , only : ancmsg, msgwarning, msgerror, aninfo_blind_1
+          use constant_mod , only : zero, em20, ep20
+          use precision_mod, only : WP
+          use seatbelt_mod, only : nguided_cable,guide
+! ----------------------------------------------------------------------------------------------------------------------
+!                                                   Included files
+! ----------------------------------------------------------------------------------------------------------------------
+          implicit none
+#include "units_c.inc"
+! ----------------------------------------------------------------------------------------------------------------------
+!                                                   Arguments
+! ----------------------------------------------------------------------------------------------------------------------
+          integer, intent(in) :: nspmd                                     !< number of spmd domains
+! ----------------------------------------------------------------------------------------------------------------------
+!                                                   Local variables
+! ----------------------------------------------------------------------------------------------------------------------
+          integer :: i,ig, p
+          integer :: node1, node2, anchor_node
+          integer :: sum_proc(nspmd)        
+! ----------------------------------------------------------------------------------------------------------------------
+!                                                   External Functions
+! ----------------------------------------------------------------------------------------------------------------------
+          integer,external :: nlocal
+! ----------------------------------------------------------------------------------------------------------------------
+!   
+! ----------------------------------------------------------------------------------------------------------------------
+!                                                   Body
+! ----------------------------------------------------------------------------------------------------------------------
+!          
+!-------------------------------------------------------------------------------------------          
+!         Stick anchor nodes on the same processor than guide
+!-------------------------------------------------------------------------------------------   
+!                  
+          do ig = 1, nguided_cable
+!
+            sum_proc(1:nspmd) = 0             
+            do i = guide(ig)%ncont+1, guide(ig)%nb_nodes
+              node1 = guide(ig)%nodes(i)
+              do p = 1, nspmd
+                if (nlocal(node1, p) == 1) sum_proc(p) = sum_proc(p) + 1
+              end do
+            end do
+!
+            guide(ig)%proc = 0
+            do p = 1, nspmd
+              if (sum_proc(p)==guide(ig)%nb_nodes - guide(ig)%ncont) guide(ig)%proc = p
+            end do 
+!
+            do i = 1, guide(ig)%ncont
+              anchor_node = guide(ig)%cont(i)%anchor_node
+              if ((nlocal(anchor_node, guide(ig)%proc) == 0)) then
+                ! anchor node must be stick on the proc of the guide
+                call ifrontplus(anchor_node, guide(ig)%proc)
+              end if
+            end do
+!            
+          end do
+!
+! ----------------------------------------------------------------------------------------------------------------------
+        end subroutine ini_guided_cable
+      end module ini_guided_cable_mod
