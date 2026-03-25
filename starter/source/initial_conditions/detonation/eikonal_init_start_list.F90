c65242ed851b3761d200623f2deccfac05184069
Thierry Schwoertzig <104987175+Schwoertzig@users.noreply.github.com>
2026-03-25 10:25:16 +0100

    Detonators with Fast Marching Method : enhanced intialization

--- starter/source/initial_conditions/detonation/eikonal_init_start_list.F90
+++ starter/source/initial_conditions/detonation/eikonal_init_start_list.F90
@@ -21,11 +21,11 @@
 !Copyright>        software under a commercial license.  Contact Altair to discuss further if the
 !Copyright>        commercial version may interest you: https://www.altair.com/radioss/.
 !||====================================================================
-!||    eikonal_init_start_list_2d_mod   ../starter/source/initial_conditions/detonation/eikonal_init_start_list_2d.F90
+!||    eikonal_init_start_list_mod      ../starter/source/initial_conditions/detonation/eikonal_init_start_list.F90
 !||--- called by ------------------------------------------------------
 !||    eikonal_fast_marching_method     ../starter/source/initial_conditions/detonation/eikonal_fast_marching_method.F90
 !||====================================================================
-      module eikonal_init_start_list_2d_mod
+      module eikonal_init_start_list_mod
       implicit none
       contains
 ! ======================================================================================================================
@@ -34,16 +34,16 @@
 !! \brief initialize narrow band for fast marching method depending on user input
 !! \details
 !||====================================================================
-!||    eikonal_init_start_list_2d          ../starter/source/initial_conditions/detonation/eikonal_init_start_list_2d.F90
+!||    eikonal_init_start_list             ../starter/source/initial_conditions/detonation/eikonal_init_start_list.F90
 !||--- called by ------------------------------------------------------
 !||    eikonal_fast_marching_method        ../starter/source/initial_conditions/detonation/eikonal_fast_marching_method.F90
 !||--- calls      -----------------------------------------------------
 !||--- uses       -----------------------------------------------------
 !||    detonators_mod                      ../starter/share/modules1/detonators_mod.F
 !||====================================================================
-        subroutine eikonal_init_start_list_2d(nstart, start_elem_list, start_elem_tdet, detonators, numel, numnod, &
+        subroutine eikonal_init_start_list(nstart, start_elem_list, start_elem_tdet, detonators, numel, numnod, &
           nvois, nod2el, knod2el, ale_connectivity, elem_list_bij, neldet, xel, x, &
-          nix, ix, mat_det, vel, uelem_list)
+          nix, ix, mat_det, vel, uelem_list, elem_list)
 ! ----------------------------------------------------------------------------------------------------------------------
 !                                                   Modules
 ! ----------------------------------------------------------------------------------------------------------------------
@@ -70,17 +70,18 @@
           type (detonators_struct_),intent(in) :: detonators
           integer,intent(in) :: nvois !< max number of adjacent elems
           integer,intent(in) :: nod2el(nvois*numel) !< connectivity node->quad
-          integer,intent(in) :: knod2el(numnod+1) !< quad_id -> index in nod2el (size 4)
+          integer,intent(in) :: knod2el(1:numnod+1) !< quad_id -> index in nod2el (size 4)
           type (t_ale_connectivity), intent(inout) :: ale_connectivity
-          integer,intent(in) :: elem_list_bij(numel)
           integer,intent(in) :: neldet
+          integer,intent(in) :: elem_list_bij(numel) !< global to local
+          integer,intent(in) :: uelem_list(neldet) !< user ids
+          integer,intent(in) :: elem_list(neldet) !< local to global
           real(kind=WP),intent(in) :: xel(3,neldet)
           real(kind=WP),intent(in) :: x(3,numnod)
           integer,intent(in) :: nix
           integer,intent(in) :: ix(nix,numel)
           integer,intent(in) :: mat_det
           real(kind=WP),intent(in) :: vel(neldet)
-          integer,intent(in) :: uelem_list(neldet) !< array size
 ! ----------------------------------------------------------------------------------------------------------------------
 !                                                   Local variables
 ! ----------------------------------------------------------------------------------------------------------------------
@@ -88,12 +89,15 @@
           integer :: ii,jj,ielem
           integer :: I_shadow_flag
           integer :: inod, nnod, nod_id
-          integer :: num_adj, adjacent_elem(64) ! Maximum number of elements in diagonal neighborhood
+          integer :: num_adj, num_adj2
+          integer, allocatable,dimension(:) :: adjacent_elem
+          integer :: adjacent_elem2(64)
           integer,allocatable,dimension(:) :: itag_elem
           integer :: iad1,lgth !< variable for ale_connectivity elem-elem buffer
           integer :: iev, iel, ie
           real(kind=WP) :: dx,dy,dz,dl,tdet,dcj
-          real(kind=WP) :: xdet, ydet,zdet
+          real(kind=WP) :: xdet, ydet, zdet !< detonator location
+          real(kind=WP) :: r0  !optionnal initialization radius
           integer, allocatable, dimension(:):: indx
           integer, allocatable, dimension(:) :: int_tmp_array
           integer,allocatable,dimension(:) :: start_elem_uid     !< user identifier for sorting
@@ -104,6 +108,7 @@
 ! ----------------------------------------------------------------------------------------------------------------------
           allocate(itag_elem(numel)) !tag to check if elem was already found
           allocate(tmp_tdet(neldet))
+          allocate(adjacent_elem(numel))
           tmp_tdet(:) = ep21
           ! loop over detonation point with shadowing option (I_shadow_flag=1)
           ! and build ADJACENT NEIGHBORHOOD (3-STAGE-PROCESS)
@@ -115,60 +120,154 @@
             ! loop over detonation points
             do inod = 1,nnod
               nod_id = detonators%point(idet)%nodlist(inod)
-              adjacent_elem(1:64) = 0
-              num_adj = 0 !max 64
-              ! --- FIRST STAGE
-              ! loop over attached elems
-              do ii=Knod2el(nod_id)+1,Knod2el(nod_id+1)
-                ielem = nod2el(ii)
-                if(itag_elem(ielem) == 0)then
-                  if(mat_det /=0 .and. ix(1,ielem) /= mat_det)cycle
-                  itag_elem(ielem) = 100
-                  num_adj = num_adj + 1
-                  adjacent_elem(num_adj) = ielem
-                end if
-              end do
-              ! --- SECOND STAGE
-              ! Manathan neighborhood
-              do ii=1,num_adj
-                ie = adjacent_elem(ii)
-                iad1 = ale_connectivity%ee_connect%iad_connect(ie)
-                lgth = ale_connectivity%ee_connect%iad_connect(ie+1) - iad1
-                do jj=1,lgth
-                  iev = ale_connectivity%ee_connect%connected(iad1 + jj - 1)
-                  if(iev == 0)cycle
-                  iel = elem_list_bij(iev)
-                  if(itag_elem(iev) == 0)then
-                    if(mat_det /=0 .and. ix(1,iev) /= mat_det)cycle
-                    itag_elem(iev) = 10
+              r0 = detonators%point(idet)%r0
+              if(r0 == zero)then
+                ! INIT METHOD : Manathan neighborhood initialization
+                adjacent_elem(1:256) = 0
+                num_adj = 0
+                ! 1st STAGE : mark with 1000
+                ! 2nd STAGE : mark with  100
+                ! 3rd STAGE : mark with   10
+                ! 4th STAGE : mark with   +1 (only if <10)
+                ! 5th STAGE : mark with   +1 (only if <2)
+                ! result keep elem with TAG > 2 => double layer Manathan neighborhood
+                !   use to initialize with Spherical detonation wave
+                !   intialization with a single element lead to a large numerical error near the detonation point (which will be propagated especially in diagonal direction)
+                !   +---------+---------+---------+---------+
+                !   |         |         |         |     +1  |
+                !   +---------+---------+---------+---------+
+                !   |         |      2  |      2  |     10  |
+                !   +---------+---------+---------+---------+
+                !   |         |      2  |     10  |    100  |
+                !   +---------+---------+---------+---------+
+                !   |      +1 |     10  |    100  |   1000  |
+                !   +---------+---------+---------+---------O  <---- Det point
+
+
+
+                ! --- FIRST STAGE
+                ! loop over attached elems
+                 do ii=Knod2el(nod_id)+1,Knod2el(nod_id+1) ! loop over elems connected to nod_id
+                  ielem = nod2el(abs(ii))
+                  if(itag_elem(ielem) == 0)then
+                    if(mat_det /=0 .and. ix(1,ielem) /= mat_det)cycle
+                    itag_elem(ielem) = 1000
                     num_adj = num_adj + 1
-                    adjacent_elem(num_adj) = iev
+                    adjacent_elem(num_adj) = ielem
                   end if
                 end do
-              end do
-              ! --- THIRD STAGE
-              ! Adjacent neighborhood
-              do ii=1,num_adj
-                ie = adjacent_elem(ii)
-                if(itag_elem(ie) /= 10)cycle
-                iad1 = ale_connectivity%ee_connect%iad_connect(ie)
-                lgth = ale_connectivity%ee_connect%iad_connect(ie+1) - iad1
-                do jj=1,lgth
-                  iev = ale_connectivity%ee_connect%connected(iad1 + jj - 1)
-                  if(iev == 0)cycle
-                  iel = elem_list_bij(iev)
-                  if(itag_elem(iev) < 10)then
-                    if(mat_det /=0 .and. ix(1,iev) /= mat_det)cycle
-                    itag_elem(iev) = itag_elem(iev) + 1
+                ! --- SECOND STAGE
+                ! Adjacent elems (face to face)
+                do ii=1,num_adj
+                  ie = adjacent_elem(ii)
+                  if(itag_elem(ie) /= 1000)cycle
+                  iad1 = ale_connectivity%ee_connect%iad_connect(ie)
+                  lgth = ale_connectivity%ee_connect%iad_connect(ie+1) - iad1
+                  do jj=1,lgth
+                    iev = ale_connectivity%ee_connect%connected(iad1 + jj - 1)
+                    if(iev == 0)cycle
+                    iel = elem_list_bij(iev)
+                    if(itag_elem(iev) == 0)then
+                      if(mat_det /=0 .and. ix(1,iev) /= mat_det)cycle
+                      itag_elem(iev) = 100
+                      num_adj = num_adj + 1
+                      adjacent_elem(num_adj) = iev
+                    end if
+                  end do
+                end do
+                ! --- THIRD STAGE
+                ! Adjacent elems (face to face)
+                do ii=1,num_adj
+                  ie = adjacent_elem(ii)
+                  if(itag_elem(ie) /= 100)cycle
+                  iad1 = ale_connectivity%ee_connect%iad_connect(ie)
+                  lgth = ale_connectivity%ee_connect%iad_connect(ie+1) - iad1
+                  do jj=1,lgth
+                    iev = ale_connectivity%ee_connect%connected(iad1 + jj - 1)
+                    if(iev == 0)cycle
+                    iel = elem_list_bij(iev)
+                    if(itag_elem(iev) == 0)then
+                      if(mat_det /=0 .and. ix(1,iev) /= mat_det)cycle
+                      itag_elem(iev) = 10
+                      num_adj = num_adj + 1
+                      adjacent_elem(num_adj) = iev
+                    end if
+                  end do
+                end do
+                ! --- FOURTH STAGE
+                ! Adjacent elems (face to face)
+                num_adj2 = 0
+                do ii=1,num_adj
+                  ie = adjacent_elem(ii)
+                  if(itag_elem(ie) /= 10)cycle
+                  iad1 = ale_connectivity%ee_connect%iad_connect(ie)
+                  lgth = ale_connectivity%ee_connect%iad_connect(ie+1) - iad1
+                  do jj=1,lgth
+                    iev = ale_connectivity%ee_connect%connected(iad1 + jj - 1)
+                    if(iev == 0)cycle
+                    iel = elem_list_bij(iev)
+                    if(itag_elem(iev) < 2 )then
+                      if(mat_det /=0 .and. ix(1,iev) /= mat_det)cycle
+                      itag_elem(iev) = itag_elem(iev) + 1
+                      if(itag_elem(iev) == 2)then
+                        num_adj2 = num_adj2 + 1
+                        adjacent_elem2(num_adj2) = iev
+                      end if
+                    end if
+                  end do
+                end do
+                ! --- FIFTH STAGE
+                ! Adjacent elems (face to face)
+                do ii=1,num_adj2
+                  ie = adjacent_elem2(ii)
+                  if(itag_elem(ie) >= 10 .or. itag_elem(ie) == 1)cycle
+                  iad1 = ale_connectivity%ee_connect%iad_connect(ie)
+                  lgth = ale_connectivity%ee_connect%iad_connect(ie+1) - iad1
+                  do jj=1,lgth
+                    iev = ale_connectivity%ee_connect%connected(iad1 + jj - 1)
+                    if(iev == 0)cycle
+                    iel = elem_list_bij(iev)
+                    if(itag_elem(iev) < 2 )then
+                      if(mat_det /=0 .and. ix(1,iev) /= mat_det)cycle
+                      itag_elem(iev) = itag_elem(iev) + 1
+                     end if
+                  end do
+                end do
+                ! --- LAST STAGE
+                ! Manathan neighborhood
+                do ii=1,numel
+                  if(itag_elem(ii) > 1 .and. itag_elem(ii) < 10)then
+                    num_adj = num_adj + 1
+                    adjacent_elem(num_adj) = ii
                   end if
                 end do
-              end do
-              do ii=1,numel
-                if(itag_elem(ii) == 2)then
-                  num_adj = num_adj + 1
-                  adjacent_elem(num_adj) = ii
-                end if
-              end do
+
+              elseif(r0 /= zero)then
+              ! INIT METHOD : SPHERICAL WAVE WITHIN RADIUS
+              ! SELECT ALL THE ELEMENTS WITHIN A RADIUS R0 FROM THE DETONATION POINT
+                 adjacent_elem(1:numel) = 0
+                 num_adj = 0
+                 itag_elem(:) = 0
+                 !select all in the given radius, set itag_elem to 2
+                 !matid = detonators%point(idet)%mat
+                 xdet = x(1,nod_id)
+                 ydet = x(2,nod_id)
+                 zdet = x(3,nod_id)
+                 do ii=1,neldet
+                     iel = elem_list(ii) ! local to global
+                     dx = xdet - xel(1,iel)
+                     dy = ydet - xel(2,iel)
+                     dz = zdet - xel(3,iel)
+                     dl = sqrt(dx*dx + dy*dy + dz*dz)
+                     if(dl <= r0)then
+                        itag_elem(ii) = 1
+                        num_adj = num_adj + 1
+                        ! iel = elem_list_bij(iev)  !global to local
+                        adjacent_elem(num_adj) = ii
+                     end if
+                 end do
+
+              end if
 
               ! ---INIT WITH RADIAL DISTANCE
               dcj = zero
@@ -251,7 +350,8 @@
           !remaing deallocate
           deallocate(itag_elem)
           deallocate(tmp_tdet)
+          deallocate(adjacent_elem)
 
-        end subroutine eikonal_init_start_list_2d
+        end subroutine eikonal_init_start_list
 ! ----------------------------------------------------------------------------------------------------------------------
-      end module eikonal_init_start_list_2d_mod
+      end module eikonal_init_start_list_mod
