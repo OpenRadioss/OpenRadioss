OpenRadioss/OpenRadioss

commit 9bc12a8f16318b336ae3393971de9ab973e57cd8

add new rigid option in /PART
modif after copilot

starter/source/constraints/general/rbody/hm_read_rbody.F
@@ -52,7 +52,7 @@ Copyright>        commercial version may interest you: https://www.altair.com/ra
      3                         SENSORS   ,IMERGE  ,UNITAB  ,ISKN     ,NOM_OPT  ,
      4                         NUMSL     ,KNOD2ELS,KNOD2ELC,KNOD2ELTG,KNOD2EL1D,
      5                         KNOD2ELQ  ,ITAGND  ,ICDNS10 ,LSUBMODEL,ICFIELD  ,
-     6                         LCFIELD   )
+     6                         LCFIELD   ,SLRBODY ,N_NRBODY,NRBODY_TO_PART)
 C-------------------------------------
 C     READING STRUCTURE RIGIDES 
 C-----------------------------------------------
@@ -67,6 +67,7 @@ C-----------------------------------------------
       USE HM_OPTION_READ_MOD
       USE SENSOR_MOD
       USE NAMES_AND_TITLES_MOD , ONLY : NCHARTITLE, NCHARKEY
+      USE rbody_part_modif_mod, ONLY : rbody_part_modif
 C-----------------------------------------------
 C   I m p l i c i t   T y p e s
 C-----------------------------------------------
@@ -90,7 +91,9 @@ C-----------------------------------------------
 C   D u m m y   A r g u m e n t s
 C-----------------------------------------------
       TYPE (UNIT_TYPE_),INTENT(IN) ::UNITAB 
-      INTEGER NPBY(NNPBY,*), LPBY(*), ITAB(*), ITABM1(*)
+      INTEGER ,INTENT(IN) :: N_NRBODY,SLRBODY
+      INTEGER , DIMENSION(N_NRBODY), INTENT(IN) :: NRBODY_TO_PART
+      INTEGER NPBY(NNPBY,*), LPBY(SLRBODY), ITAB(*), ITABM1(*)
       INTEGER IBFV(NIFV,*)
       INTEGER IGRV(NIGRV,*),IBGR(*),IMERGE(*),
      .        ISKN(LISKN,*),NUMSL,
@@ -111,6 +114,7 @@ C-----------------------------------------------
      .   JC,UID,IFLAGUNIT,SUB_INDEX,NRB,
      .   IFAIL,NRB_R2R
       INTEGER IDSURF, ISU, NN, IAD, M, IOPT, IEXPAMS, NEL
+      INTEGER PARENT_OF(NRBODY),NRBYKIN0
       CHARACTER MESS*40
       CHARACTER(LEN=NCHARTITLE)::TITR,TITR1
       CHARACTER(LEN=NCHARKEY)::KEY
@@ -145,6 +149,8 @@ C   16 : NUMBER OF SECONDARY NODES WITH MERGING FLAG = 3
 C   17 : IKREM
 C   18 : IFAIL
 C   19 : INITIAL NUMBER OF SECONDARY NODES (NSN_G)
+C   20 : RIGID BODY HIERARCHY LEVEL
+C   21 : PART_ID (IF PART DEFINED)
 C-----------------------------------------------
 C   RBY(NRBY,NRBYKIN), NRBY=25
 C   LOADED DURING READING       AFTER INITIALIZATION (including in RD ENGINE)
@@ -180,6 +186,8 @@ C
       K=0
       NRB=0
       NRB_R2R=0
+      PARENT_OF=0
+      NRBYKIN0 = NRBYKIN-N_NRBODY
 C
       DO N=1,NRBODY
 C
@@ -303,8 +311,17 @@ C
         CALL ANODSET(NPBY(1,NRB), CHECK_RB_M)
 C
         NPBY(11,NRB)=K
+        NPBY(6,NRB)=ID
         M = NPBY(1,NRB)
         NSL = NODGRNR6(M,IGU,IGS,LPBY(K+1),IGRNOD,ITABM1,MESS,ID)
+        IF (NRB>NRBYKIN0) THEN
+          CALL RBODY_PART_MODIF(NRBYKIN ,NNPBY ,NPBY  ,SLRBODY ,LPBY  ,          
+     .                           NUMNOD ,NRB   ,NSL   ,LPBY(K+1:K+NSL),
+     .                           NRBYKIN0,PARENT_OF)
+          NPBY(21,NRB)= NRBODY_TO_PART(NRB-NRBYKIN0) 
+        ELSE
+          NPBY(21,NRB)= 0 
+        END IF
 c
         DO I=1,NSL
           ITAG(LPBY(K+I)) = 1
@@ -352,7 +369,6 @@ C
           IKREM=1
         ENDIF
         NPBY(5,NRB)=ISPHER
-        NPBY(6,NRB)=ID
         NPBY(17,NRB)=IKREM
         IF(ISENS == 0)THEN
           NPBY(7,NRB)=1

starter/source/starter/lectur.F
@@ -552,7 +552,7 @@ Copyright>        commercial version may interest you: https://www.altair.com/ra
       SUBROUTINE LECTUR(MULTI_FVM                   ,LSUBMODEL          ,IS_DYNA         ,DETONATORS   ,EBCS_TAB,
      .                  SEATBELT_CONVERTED_ELEMENTS ,NB_SEATBELT_SHELLS ,NB_DYNA_INCLUDE ,USER_WINDOWS ,OUTPUT  ,
      .                  MAT_ELEM,NAMES_AND_TITLES,DEFAULTS,glob_therm,pblast,
-     .                  SENSOR_USER_STRUCT,CHECK_USED)
+     .                  SENSOR_USER_STRUCT,CHECK_USED,N_NRBODY,NRBODY_TO_PART)
 C-----------------------------------------------
 C   M o d u l e s
 C-----------------------------------------------
@@ -668,6 +668,7 @@ C-----------------------------------------------
       use get_element_group_mod , only : get_element_group
       use elm_group_mod , only : elm_group_
       use my_real_tot_int_transfer_mod , only : my_real_tot_int_transfer
+      use rbody_part_modif_mod,     ONLY : rbody_part_check,rpart_inivel_check
 C-----------------------------------------------
 C   I m p l i c i t   T y p e s
 C-----------------------------------------------
@@ -691,6 +692,8 @@ C-----------------------------------------------
       TYPE(PBLAST_),INTENT(INOUT)           :: PBLAST
       TYPE(SENSOR_USER_STRUCT_)              :: SENSOR_USER_STRUCT
       INTEGER,INTENT(IN)                     :: CHECK_USED                          !< Flag to check if node is used
+      INTEGER,INTENT(IN)                     :: N_NRBODY                            !< Number of rigid bodies by part
+      INTEGER,DIMENSION(N_NRBODY),INTENT(IN) :: NRBODY_TO_PART                      !< rigid body to part mapping
 C-----------------------------------------------
 C   G l o b a l   P a r a m e t e r s
 C-----------------------------------------------
@@ -6904,6 +6907,8 @@ C--------------------------------------------
        ALLOCATE(ITAGND(0),STAT=stat)
       END IF
 
+!     replace old /MAT/RIGID by RBODY by part	  
+      IF (N_NRBODY>0) IRIGID_MAT = 0
 C--------------------------------------------
 C     Domain Decomposition 1 (Reconstruction of Tables)
 C--------------------------------------------
@@ -6952,7 +6957,7 @@ C--------------------------------------------
       ERR_CATEGORY='GRAVITY'
       CALL TRACE_IN1(ERR_MSG,LEN_TRIM(ERR_MSG))
 c      CALL PRELECGRAV(NUMGRAV ,IGRNOD)
-      CALL HM_PREREAD_GRAV(NUMGRAV ,IGRNOD , LSUBMODEL)
+      CALL HM_PREREAD_GRAV(NUMGRAV ,IGRNOD , LSUBMODEL,N_NRBODY)
       SIGRV = NIGRV*NGRAV
       SLGRAV = NUMGRAV
       IF(IDDLEVEL==0)THEN
@@ -8030,7 +8035,7 @@ C
      3       SENSORS   ,IMERGE  ,UNITAB  ,ISKWN   ,NOM_OPT   ,
      4       SLRBODY   ,KNOD2ELS,KNOD2ELC,KNOD2ELTG,KNOD2EL1D,
      5       KNOD2ELQ  ,ITAGND  ,ICNDS10 ,LSUBMODEL,ICFIELD  ,
-     6       LCFIELD   )
+     6       LCFIELD   ,SLRBODY ,N_NRBODY,NRBODY_TO_PART)
         ENDIF
 C--------------------------------------------
 C     Reading rigid body merges
@@ -8059,6 +8064,22 @@ C--------------------------------------------
      2       D      ,IDDLEVEL,NOM_OPT,SLRBODY,
      3       ROOTNAM,ROOTLEN )
       ENDIF
+C--------------------------------------------
+C     Checking rigid part
+C--------------------------------------------
+      IF(N_NRBODY > 0) THEN
+        CALL RBODY_PART_CHECK(NRBYKIN ,NNPBY ,NPBY  ,SLRBODY ,LPBY  ,       
+     .                        NUMNOD  ,ITAB  ,NPART ,IPART   ,LIPART1,       
+     .                        ICODE   ,ISKEW ,NFXVEL,NIFV    ,IBFV ,
+     .                        NGRAV   ,NIGRV ,IGRV  ,SLGRAV  ,LGRAV) 
+        CALL RPART_INIVEL_CHECK(NRBYKIN ,NNPBY ,NPBY  ,SLPBY    ,LPBY   ,        
+     .                        HM_NINVEL ,NUMNOD,ITABM1,LSUBMODEL,UNITAB ,        
+     .                        NTRANSF   ,NRTRANS,RTRANS,RBY_INIAXIS,ISKWN,     
+     .                         LISKN    ,NUMSKW ,LSKEW  ,SKEW   ,IFRAME  ,        
+     .                         NUMFRAM  ,NXFRAME,XFRAME ,NGRNOD ,IGRNOD  , 
+     .                         N2D      ,X      ,V     ,VR  )
+
+      END IF
 C--------------------------------------------
 C     READING RB LAGRANGE
 C--------------------------------------------

hm_cfg_files/config/CFG/radioss2612/PART/part.cfg
@@ -1,0 +1,85 @@
+//Copyright>    CFG Files and Library ("CFG")
+//Copyright>    Copyright (C) 1986-2026 Altair Engineering Inc.
+//Copyright>
+//Copyright>    Altair Engineering Inc. grants to third parties limited permission to
+//Copyright>    use and modify CFG solely in connection with OpenRadioss software, provided
+//Copyright>    that any modification to CFG by a third party must be provided back to
+//Copyright>    Altair Engineering Inc. and shall be deemed a Contribution under and therefore
+//Copyright>    subject to the CONTRIBUTOR LICENSE AGREEMENT for OpenRadioss software.
+//Copyright>
+//Copyright>    CFG IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+//Copyright>    INCLUDING, BUT NOT LIMITED TO, THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR
+//Copyright>    A PARTICULAR PURPOSE, AND NONINFRINGEMENT.  IN NO EVENT SHALL ALTAIR ENGINEERING
+//Copyright>    INC. OR ITS AFFILIATES BE LIABLE FOR ANY CLAIM, DAMAGES, OR OTHER LIABILITY,
+//Copyright>    WHETHER IN AN ACTION OF CONTRACT, TORT, OR OTHERWISE, ARISING FROM, OUT OF, OR
+//Copyright>    IN CONNECTION WITH CFG OR THE USE OR OTHER DEALINGS IN CFG.
+//
+// Part Setup File
+// 
+
+ATTRIBUTES {
+  materialid = VALUE(MATERIAL, "Material");
+  propertyid = VALUE(PROPERTY,"Property");
+  subsetid = VALUE(ASSEMBLY,"Subset");
+  THICK = VALUE(FLOAT,"Thickness");
+  IRIGID = VALUE(INT, "");
+  TITLE = VALUE(STRING, "");
+  Node_ID = VALUE(NODE, "Node ID");
+  Skew_ID = VALUE(SYSTEM, "Skew ID");
+  Mass = VALUE(FLOAT, "Mass");
+  Sens_ID = VALUE(SENSOR, "Sensitivity ID");
+  JXX = VALUE(FLOAT, "Jxx");
+  JYY = VALUE(FLOAT, "Jyy");
+  JZZ = VALUE(FLOAT, "Jzz");
+  JXY = VALUE(FLOAT, "Jxy");
+  JYZ = VALUE(FLOAT, "Jyz");
+  JXZ = VALUE(FLOAT, "Jxz");
+}
+
+GUI(COMMON) {
+  optional:  
+  SCALAR(THICK) {DIMENSION="l";}
+  SCALAR(Mass) {DIMENSION="m";}
+  SCALAR(JXX) {DIMENSION="massmi";}
+  SCALAR(JYY) {DIMENSION="massmi";}
+  SCALAR(JZZ) {DIMENSION="massmi";}
+  SCALAR(JXY) {DIMENSION="massmi";}
+  SCALAR(JYZ) {DIMENSION="massmi";}
+  SCALAR(JXZ) {DIMENSION="massmi";}
+}
+
+// File format for Radioss 2026
+FORMAT(radioss2612) 
+{
+    HEADER("/PART/%d",_ID_);
+    CARD("%-100s",TITLE);
+    COMMENT("#  prop_ID    mat_ID subset_ID               Thick    Irigid");
+    CARD("%10d%10d%10d%20lg%10d",propertyid,materialid,subsetid,THICK,IRIGID);
+
+    if (IRIGID == 1) {
+      COMMENT("#  Node_ID   Skew_ID                Mass             Sens_ID");
+      CARD("%10d%10d%20lg%10s%10d",Node_ID,Skew_ID,Mass,_BLANK_,Sens_ID);
+      COMMENT("#                Jxx                 Jyy                 Jzz");
+      CARD("%20lg%20lg%20lg",JXX,JYY,JZZ);
+      COMMENT("#                Jxy                 Jyz                 Jxz");
+      CARD("%20lg%20lg%20lg",JXY,JYZ,JXZ);
+    }
+}
+
+// File format for Radioss 51
+FORMAT(radioss51) 
+{
+    HEADER("/PART/%d",_ID_);
+    CARD("%-100s",TITLE);
+    COMMENT("#  prop_ID    mat_ID subset_ID               Thick");
+    CARD("%10d%10d%10d%20lg",propertyid,materialid,subsetid,THICK);
+}
+
+// File format for Radioss 41
+FORMAT(radioss41) 
+{
+    HEADER("/PART/%d/%-40s",_ID_,TITLE);
+    COMMENT("#prop_ID  mat_ID  subset           Thick");
+    CARD("%8d%8d%8d%16lg",propertyid,materialid,subsetid,THICK);
+}
+

starter/source/constraints/general/rbody/checkrby.F
@@ -74,7 +74,7 @@ C   L o c a l   V a r i a b l e s
 C-----------------------------------------------
       INTEGER I,J,K,KK,N,NSL,
      .   ISPHER,IDIR,ID,ICDG,NSL_XTRA,
-     .   NRB,NUMSL_TMP,IFOUND,IM_ID(NRBYKIN),NM,IM,IACTI
+     .   NRB,NUMSL_TMP,IFOUND,IM_ID(NRBYKIN),NM,IM,IACTI,NUMSL1
       CHARACTER(LEN=NCHARTITLE)::TITR
       INTEGER, DIMENSION(:,:), ALLOCATABLE :: TABSL
       INTEGER, DIMENSION(:), ALLOCATABLE :: INDEX,IKINE1
@@ -99,9 +99,10 @@ C   NPBY(14,N) = NUMBER OF XTRA_NODE with Iflag=1
 C   NPBY(15,N) = NUMBER OF XTRA_NODE with Iflag=2
 C   NPBY(16,N) = NUMBER OF XTRA_NODE with Iflag=3
 C=======================================================================
-      IF (NUMSL > 0) THEN
-        ALLOCATE(TABSL(2,NUMSL))
-        ALLOCATE(INDEX(3*NUMSL))
+      NUMSL1 = NPBY(2,NRBYKIN) + NPBY(11,NRBYKIN) ! effective number<allocated size due to reductions
+      IF (NUMSL1 > 0) THEN
+        ALLOCATE(TABSL(2,NUMSL1))
+        ALLOCATE(INDEX(3*NUMSL1))
         TABSL=0
         INDEX=0
       END IF

commit dd9ed8f5d9e7020e07a1adfd4466f583e91c0081

regression by 9bc12a8f16318b336ae3393971de9ab973e57cd8

starter/source/constraints/general/rbody/hm_read_rbody.F
@@ -54,7 +54,7 @@ Copyright>        commercial version may interest you: https://www.altair.com/ra
      3                         SENSORS   ,IMERGE  ,UNITAB  ,ISKN     ,NOM_OPT  ,
      4                         NUMSL     ,KNOD2ELS,KNOD2ELC,KNOD2ELTG,KNOD2EL1D,
      5                         KNOD2ELQ  ,ITAGND  ,ICDNS10 ,LSUBMODEL,ICFIELD  ,
-     6                         LCFIELD   ,SLRBODY ,N_NRBODY,NRBODY_TO_PART)
+     6                         LCFIELD   ,SLRBODY ,N_NRBODY,NRBODY_TO_PART,IPART)
 C-------------------------------------
 C     READING STRUCTURE RIGIDES 
 C-----------------------------------------------
@@ -103,6 +103,7 @@ C-----------------------------------------------
      .        ITAGND(*),ICDNS10(*), ICFIELD(SIZFIELD,*), LCFIELD(*)
       my_real RBY(NRBY,*)
       INTEGER NOM_OPT(LNOPT1,*)
+      INTEGER ,DIMENSION(LIPART1,NPART),INTENT(IN) :: IPART
 C-----------------------------------------------
       TYPE (GROUP_)  , DIMENSION(NGRNOD)  :: IGRNOD
       TYPE (SURF_)   ,TARGET, DIMENSION(NSURF)   :: IGRSURF        
@@ -114,7 +115,7 @@ C-----------------------------------------------
       INTEGER I, J, K, N, NSL, NSL0, NSKEW, IC,
      .   ISPHER, IGU,IGS,ISENS,ID,ICDG,
      .   JC,UID,IFLAGUNIT,SUB_INDEX,NRB,
-     .   IFAIL,NRB_R2R
+     .   IFAIL,NRB_R2R,PART_ID,NP
       INTEGER IDSURF, ISU, NN, IAD, M, IOPT, IEXPAMS, NEL
       INTEGER PARENT_OF(NRBODY),NRBYKIN0
       CHARACTER MESS*40
@@ -320,7 +321,15 @@ C
           CALL RBODY_PART_MODIF(NRBYKIN ,NNPBY ,NPBY  ,SLRBODY ,LPBY  ,          
      .                           NUMNOD ,NRB   ,NSL   ,LPBY(K+1:K+NSL),
      .                           NRBYKIN0,PARENT_OF)
-          NPBY(21,NRB)= NRBODY_TO_PART(NRB-NRBYKIN0) 
+          PART_ID = NRBODY_TO_PART(NRB-NRBYKIN0)
+          NP = 0  
+          DO J=1,NPART
+            IF(PART_ID == IPART(4,J)) THEN
+              NP=J
+              EXIT
+            ENDIF
+          END DO
+          NPBY(21,NRB)= NP 
         ELSE
           NPBY(21,NRB)= 0 
         END IF

starter/source/constraints/general/rbody/rbody_part_modif.F90
@@ -21,11 +21,6 @@
 !Copyright>        software under a commercial license.  Contact Altair to discuss further if the
 !Copyright>        commercial version may interest you: https://www.altair.com/radioss/.
 !||====================================================================
-!||    rbody_part_modif_mod   ../starter/source/constraints/general/rbody/rbody_part_modif.F90
-!||--- called by ------------------------------------------------------
-!||    hm_read_rbody          ../starter/source/constraints/general/rbody/hm_read_rbody.F
-!||    lectur                 ../starter/source/starter/lectur.F
-!||====================================================================
       module rbody_part_modif_mod
       contains
 ! ======================================================================================================================
@@ -33,15 +28,6 @@
 ! \detail check if slave nodes of rbody_part are also defined as slave nodes of previous rbodies
 !         if yes : remove all these nodes and add their main_id inplace
 ! ======================================================================================================================
-!||====================================================================
-!||    rbody_part_modif   ../starter/source/constraints/general/rbody/rbody_part_modif.F90
-!||--- called by ------------------------------------------------------
-!||    hm_read_rbody      ../starter/source/constraints/general/rbody/hm_read_rbody.F
-!||--- calls      -----------------------------------------------------
-!||    ancmsg             ../starter/source/output/message/message.F
-!||--- uses       -----------------------------------------------------
-!||    message_mod        ../starter/share/message_module/message_mod.F
-!||====================================================================
         subroutine rbody_part_modif(nrbykin ,nnpby ,npby  ,slpby ,lpby  ,            &
                                     numnod  ,irb   ,nsn   ,isl   ,nrbykin0,          &
                                     parent_of)
@@ -147,21 +133,11 @@
 ! ======================================================================================================================
 ! \brief check rbody by part with bcs(boundary conditions),impvel,gravity,inivel
 ! ======================================================================================================================
-!||====================================================================
-!||    rbody_part_check   ../starter/source/constraints/general/rbody/rbody_part_modif.F90
-!||--- called by ------------------------------------------------------
-!||    lectur             ../starter/source/starter/lectur.F
-!||--- calls      -----------------------------------------------------
-!||    rpart_bcs_check    ../starter/source/constraints/general/rbody/rbody_part_modif.F90
-!||    rpart_fv_check     ../starter/source/constraints/general/rbody/rbody_part_modif.F90
-!||    rpart_grav_check   ../starter/source/constraints/general/rbody/rbody_part_modif.F90
-!||--- uses       -----------------------------------------------------
-!||    message_mod        ../starter/share/message_module/message_mod.F
-!||====================================================================
         subroutine rbody_part_check(nrbykin ,nnpby ,npby  ,slpby ,lpby   ,       &
                                     numnod  ,itab  ,npart ,ipart ,lipart1,       &
                                     icode   ,iskew ,nfxvel,nifv  ,ibfv   ,       &
-                                    ngrav   ,nigrav,igrav ,slgrav,lgrav  )
+                                    ngrav   ,nigrav,igrav ,slgrav,lgrav  ,       &
+                                    numskw  )
 
 ! ----------------------------------------------------------------------------------------------------------------------
 !                                                        Modules
@@ -195,6 +171,7 @@
           integer, intent(in)                                      :: nigrav          !< first dimension of grav
           integer, intent(in)                                      :: ngrav           !< number of gravities
           integer, intent(in)                                      :: slgrav          !< first dimension of lgrav
+          integer, intent(in)                                      :: numskw          !< number of local skew
           integer, dimension(nigrav,ngrav),     intent(inout)      :: igrav           !< gravities data
           integer, dimension(slgrav),           intent(inout)      :: lgrav           !< gravities node list
 ! ----------------------------------------------------------------------------------------------------------------------
@@ -218,7 +195,7 @@
             nsl = npby(2,i)
             part_id = ipart(4,jpart)
             itag(lpby(iad+1:iad+nsl)) = 1
-            call rpart_bcs_check(m,nsl,lpby(iad+1:iad+nsl),icode,iskew,numnod,itab,part_id)
+            call rpart_bcs_check(m,nsl,lpby(iad+1:iad+nsl),icode,iskew,numnod,itab,part_id,numskw)
             call rpart_fv_check(m,itag,nfxvel,nifv,ibfv,numnod,itab,part_id)
             call rpart_grav_check(m,itag,ngrav,nigrav,igrav,slgrav,lgrav,numnod,itab,part_id)
             itag(lpby(iad+1:iad+nsl)) = 0
@@ -228,16 +205,7 @@
 ! ======================================================================================================================
 ! \brief check rbody by part with bcs(boundary conditions),replace slave nodes by main_id if needed
 ! ======================================================================================================================
-!||====================================================================
-!||    rpart_bcs_check    ../starter/source/constraints/general/rbody/rbody_part_modif.F90
-!||--- called by ------------------------------------------------------
-!||    rbody_part_check   ../starter/source/constraints/general/rbody/rbody_part_modif.F90
-!||--- calls      -----------------------------------------------------
-!||    ancmsg             ../starter/source/output/message/message.F
-!||--- uses       -----------------------------------------------------
-!||    message_mod        ../starter/share/message_module/message_mod.F
-!||====================================================================
-        subroutine rpart_bcs_check(m ,nsl ,isl  ,icode ,iskew ,numnod,itab,part_id)
+        subroutine rpart_bcs_check(m ,nsl ,isl  ,icode ,iskew ,numnod,itab,part_id,numskw)
 ! ----------------------------------------------------------------------------------------------------------------------
 !                                                        Modules
 ! ----------------------------------------------------------------------------------------------------------------------
@@ -258,6 +226,7 @@
           integer, dimension(numnod),           intent(inout)      :: iskew           !< local skew data
           integer, dimension(numnod),           intent(in)         :: itab            !< node user id 
           integer, intent(in)                                      :: part_id         !< part id
+          integer, intent(in)                                      :: numskw          !< number of local skew
 ! ----------------------------------------------------------------------------------------------------------------------
 !                                                   local variables
 ! ----------------------------------------------------------------------------------------------------------------------
@@ -273,7 +242,7 @@
             ict = ic/512  !tra_x*4 +tra_y*2 +tra_z
             icr = (ic-512*(ict))/64
             isk = iskew(ns)
-            if (isk_m/=isk) then
+            if (isk_m/=isk.and.numskw>0) then
               call ancmsg(msgid=3143,                      &
                           msgtype=msgerror,                &
                           anmode=aninfo_blind_1,           &
@@ -318,15 +287,6 @@
 ! ======================================================================================================================
 ! \brief check rbody by part with impvel(/IMPVEL,IMP/DISP...),replace slave nodes by main_id if needed
 ! ======================================================================================================================
-!||====================================================================
-!||    rpart_fv_check     ../starter/source/constraints/general/rbody/rbody_part_modif.F90
-!||--- called by ------------------------------------------------------
-!||    rbody_part_check   ../starter/source/constraints/general/rbody/rbody_part_modif.F90
-!||--- calls      -----------------------------------------------------
-!||    ancmsg             ../starter/source/output/message/message.F
-!||--- uses       -----------------------------------------------------
-!||    message_mod        ../starter/share/message_module/message_mod.F
-!||====================================================================
         subroutine rpart_fv_check(m ,itag ,nfxvel,nifv,ibfv,numnod,itab,part_id)
 ! ----------------------------------------------------------------------------------------------------------------------
 !                                                        Modules
@@ -384,15 +344,6 @@
 ! ======================================================================================================================
 ! \brief check rbody by part with /GRAV,add main_id if his 2nd nodes inside
 ! ======================================================================================================================
-!||====================================================================
-!||    rpart_grav_check   ../starter/source/constraints/general/rbody/rbody_part_modif.F90
-!||--- called by ------------------------------------------------------
-!||    rbody_part_check   ../starter/source/constraints/general/rbody/rbody_part_modif.F90
-!||--- calls      -----------------------------------------------------
-!||    ancmsg             ../starter/source/output/message/message.F
-!||--- uses       -----------------------------------------------------
-!||    message_mod        ../starter/share/message_module/message_mod.F
-!||====================================================================
         subroutine rpart_grav_check(m ,itag ,ngrav,nigrav,igrav,slgrav,lgrav,numnod,itab,part_id)
 ! ----------------------------------------------------------------------------------------------------------------------
 !                                                        Modules
@@ -460,26 +411,6 @@
 ! ======================================================================================================================
 ! \brief check rbody by part with /inivel, add main_id if his 2nd nodes inside
 ! ======================================================================================================================
-!||====================================================================
-!||    rpart_inivel_check         ../starter/source/constraints/general/rbody/rbody_part_modif.F90
-!||--- called by ------------------------------------------------------
-!||    lectur                     ../starter/source/starter/lectur.F
-!||--- calls      -----------------------------------------------------
-!||    ancmsg                     ../starter/source/output/message/message.F
-!||    hm_get_float_array_index   ../starter/source/devtools/hm_reader/hm_get_float_array_index.F
-!||    hm_get_floatv              ../starter/source/devtools/hm_reader/hm_get_floatv.F
-!||    hm_get_int_array_index     ../starter/source/devtools/hm_reader/hm_get_int_array_index.F
-!||    hm_get_intv                ../starter/source/devtools/hm_reader/hm_get_intv.F
-!||    hm_get_string              ../starter/source/devtools/hm_reader/hm_get_string.F
-!||    hm_option_read_key         ../starter/source/devtools/hm_reader/hm_option_read_key.F
-!||    hm_option_start            ../starter/source/devtools/hm_reader/hm_option_start.F
-!||    subrotvect                 ../starter/source/model/submodel/subrot.F
-!||    usr2sys                    ../starter/source/system/sysfus.F
-!||--- uses       -----------------------------------------------------
-!||    hm_option_read_mod         ../starter/share/modules1/hm_option_read_mod.F
-!||    message_mod                ../starter/share/message_module/message_mod.F
-!||    submodel_mod               ../starter/share/modules1/submodel_mod.F
-!||====================================================================
         subroutine rpart_inivel_check(nrbykin ,nnpby ,npby  ,slpby    ,lpby   ,        &
                                     hm_ninvel ,numnod,itabm1,lsubmodel,unitab ,        &
                                     ntransf   ,nrtrans,rtrans,rby_iniaxis,iskwn,       &

starter/source/starter/lectur.F
@@ -4369,7 +4369,6 @@ C PRINT /BCS
      .              IGRNOD   ,IBCSLAG ,LAG_NCF ,LAG_NKF ,LAG_NHF,
      .              IKINE1LAG,ISKWN,NOM_OPT(LNOPT1*INOM_OPT(16)+1), NBCSLAG)
       ENDIF
-
 C--------------------------------------
 C
       ALLOCATE(ICODEP(0),ISKEWP(0))
@@ -8038,7 +8037,7 @@ C
      3       SENSORS   ,IMERGE  ,UNITAB  ,ISKWN   ,NOM_OPT   ,
      4       SLRBODY   ,KNOD2ELS,KNOD2ELC,KNOD2ELTG,KNOD2EL1D,
      5       KNOD2ELQ  ,ITAGND  ,ICNDS10 ,LSUBMODEL,ICFIELD  ,
-     6       LCFIELD   ,SLRBODY ,N_NRBODY,NRBODY_TO_PART)
+     6       LCFIELD   ,SLRBODY ,N_NRBODY,NRBODY_TO_PART,IPART)
         ENDIF
 C--------------------------------------------
 C     Reading rigid body merges
@@ -8074,7 +8073,8 @@ C--------------------------------------------
         CALL RBODY_PART_CHECK(NRBYKIN ,NNPBY ,NPBY  ,SLRBODY ,LPBY  ,       
      .                        NUMNOD  ,ITAB  ,NPART ,IPART   ,LIPART1,       
      .                        ICODE   ,ISKEW ,NFXVEL,NIFV    ,IBFV ,
-     .                        NGRAV   ,NIGRV ,IGRV  ,SLGRAV  ,LGRAV) 
+     .                        NGRAV   ,NIGRV ,IGRV  ,SLGRAV  ,LGRAV,
+     .                        NUMSKW) 
         CALL RPART_INIVEL_CHECK(NRBYKIN ,NNPBY ,NPBY  ,SLPBY    ,LPBY   ,        
      .                        HM_NINVEL ,NUMNOD,ITABM1,LSUBMODEL,UNITAB ,        
      .                        NTRANSF   ,NRTRANS,RTRANS,RBY_INIAXIS,ISKWN,     