OpenRadioss/OpenRadioss

commit 9bc12a8f16318b336ae3393971de9ab973e57cd8

add new rigid option in /PART
modif after copilot

starter/source/constraints/general/rbody/hm_read_rbody.F
@@ -52,7 +52,7 @@ Copyright>        commercial version may interest you: https://www.altair.com/ra
      3                         SENSORS   ,IMERGE  ,UNITAB  ,ISKN     ,NOM_OPT  ,
      4                         NUMSL     ,KNOD2ELS,KNOD2ELC,KNOD2ELTG,KNOD2EL1D,
      5                         KNOD2ELQ  ,ITAGND  ,ICDNS10 ,LSUBMODEL,ICFIELD  ,
-     6                         LCFIELD   )
+     6                         LCFIELD   ,SLRBODY ,N_NRBODY,NRBODY_TO_PART)
 C-------------------------------------
 C     READING STRUCTURE RIGIDES 
 C-----------------------------------------------
@@ -67,6 +67,7 @@ C-----------------------------------------------
       USE HM_OPTION_READ_MOD
       USE SENSOR_MOD
       USE NAMES_AND_TITLES_MOD , ONLY : NCHARTITLE, NCHARKEY
+      USE rbody_part_modif_mod, ONLY : rbody_part_modif
 C-----------------------------------------------
 C   I m p l i c i t   T y p e s
 C-----------------------------------------------
@@ -90,7 +91,9 @@ C-----------------------------------------------
 C   D u m m y   A r g u m e n t s
 C-----------------------------------------------
       TYPE (UNIT_TYPE_),INTENT(IN) ::UNITAB 
-      INTEGER NPBY(NNPBY,*), LPBY(*), ITAB(*), ITABM1(*)
+      INTEGER ,INTENT(IN) :: N_NRBODY,SLRBODY
+      INTEGER , DIMENSION(N_NRBODY), INTENT(IN) :: NRBODY_TO_PART
+      INTEGER NPBY(NNPBY,*), LPBY(SLRBODY), ITAB(*), ITABM1(*)
       INTEGER IBFV(NIFV,*)
       INTEGER IGRV(NIGRV,*),IBGR(*),IMERGE(*),
      .        ISKN(LISKN,*),NUMSL,
@@ -111,6 +114,7 @@ C-----------------------------------------------
      .   JC,UID,IFLAGUNIT,SUB_INDEX,NRB,
      .   IFAIL,NRB_R2R
       INTEGER IDSURF, ISU, NN, IAD, M, IOPT, IEXPAMS, NEL
+      INTEGER PARENT_OF(NRBODY),NRBYKIN0
       CHARACTER MESS*40
       CHARACTER(LEN=NCHARTITLE)::TITR,TITR1
       CHARACTER(LEN=NCHARKEY)::KEY
@@ -145,6 +149,8 @@ C   16 : NUMBER OF SECONDARY NODES WITH MERGING FLAG = 3
 C   17 : IKREM
 C   18 : IFAIL
 C   19 : INITIAL NUMBER OF SECONDARY NODES (NSN_G)
+C   20 : RIGID BODY HIERARCHY LEVEL
+C   21 : PART_ID (IF PART DEFINED)
 C-----------------------------------------------
 C   RBY(NRBY,NRBYKIN), NRBY=25
 C   LOADED DURING READING       AFTER INITIALIZATION (including in RD ENGINE)
@@ -180,6 +186,8 @@ C
       K=0
       NRB=0
       NRB_R2R=0
+      PARENT_OF=0
+      NRBYKIN0 = NRBYKIN-N_NRBODY
 C
       DO N=1,NRBODY
 C
@@ -303,8 +311,17 @@ C
         CALL ANODSET(NPBY(1,NRB), CHECK_RB_M)
 C
         NPBY(11,NRB)=K
+        NPBY(6,NRB)=ID
         M = NPBY(1,NRB)
         NSL = NODGRNR6(M,IGU,IGS,LPBY(K+1),IGRNOD,ITABM1,MESS,ID)
+        IF (NRB>NRBYKIN0) THEN
+          CALL RBODY_PART_MODIF(NRBYKIN ,NNPBY ,NPBY  ,SLRBODY ,LPBY  ,          
+     .                           NUMNOD ,NRB   ,NSL   ,LPBY(K+1:K+NSL),
+     .                           NRBYKIN0,PARENT_OF)
+          NPBY(21,NRB)= NRBODY_TO_PART(NRB-NRBYKIN0) 
+        ELSE
+          NPBY(21,NRB)= 0 
+        END IF
 c
         DO I=1,NSL
           ITAG(LPBY(K+I)) = 1
@@ -352,7 +369,6 @@ C
           IKREM=1
         ENDIF
         NPBY(5,NRB)=ISPHER
-        NPBY(6,NRB)=ID
         NPBY(17,NRB)=IKREM
         IF(ISENS == 0)THEN
           NPBY(7,NRB)=1

hm_cfg_files/config/CFG/radioss2612/PART/part.cfg
@@ -1,0 +1,85 @@
+//Copyright>    CFG Files and Library ("CFG")
+//Copyright>    Copyright (C) 1986-2026 Altair Engineering Inc.
+//Copyright>
+//Copyright>    Altair Engineering Inc. grants to third parties limited permission to
+//Copyright>    use and modify CFG solely in connection with OpenRadioss software, provided
+//Copyright>    that any modification to CFG by a third party must be provided back to
+//Copyright>    Altair Engineering Inc. and shall be deemed a Contribution under and therefore
+//Copyright>    subject to the CONTRIBUTOR LICENSE AGREEMENT for OpenRadioss software.
+//Copyright>
+//Copyright>    CFG IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+//Copyright>    INCLUDING, BUT NOT LIMITED TO, THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR
+//Copyright>    A PARTICULAR PURPOSE, AND NONINFRINGEMENT.  IN NO EVENT SHALL ALTAIR ENGINEERING
+//Copyright>    INC. OR ITS AFFILIATES BE LIABLE FOR ANY CLAIM, DAMAGES, OR OTHER LIABILITY,
+//Copyright>    WHETHER IN AN ACTION OF CONTRACT, TORT, OR OTHERWISE, ARISING FROM, OUT OF, OR
+//Copyright>    IN CONNECTION WITH CFG OR THE USE OR OTHER DEALINGS IN CFG.
+//
+// Part Setup File
+// 
+
+ATTRIBUTES {
+  materialid = VALUE(MATERIAL, "Material");
+  propertyid = VALUE(PROPERTY,"Property");
+  subsetid = VALUE(ASSEMBLY,"Subset");
+  THICK = VALUE(FLOAT,"Thickness");
+  IRIGID = VALUE(INT, "");
+  TITLE = VALUE(STRING, "");
+  Node_ID = VALUE(NODE, "Node ID");
+  Skew_ID = VALUE(SYSTEM, "Skew ID");
+  Mass = VALUE(FLOAT, "Mass");
+  Sens_ID = VALUE(SENSOR, "Sensitivity ID");
+  JXX = VALUE(FLOAT, "Jxx");
+  JYY = VALUE(FLOAT, "Jyy");
+  JZZ = VALUE(FLOAT, "Jzz");
+  JXY = VALUE(FLOAT, "Jxy");
+  JYZ = VALUE(FLOAT, "Jyz");
+  JXZ = VALUE(FLOAT, "Jxz");
+}
+
+GUI(COMMON) {
+  optional:  
+  SCALAR(THICK) {DIMENSION="l";}
+  SCALAR(Mass) {DIMENSION="m";}
+  SCALAR(JXX) {DIMENSION="massmi";}
+  SCALAR(JYY) {DIMENSION="massmi";}
+  SCALAR(JZZ) {DIMENSION="massmi";}
+  SCALAR(JXY) {DIMENSION="massmi";}
+  SCALAR(JYZ) {DIMENSION="massmi";}
+  SCALAR(JXZ) {DIMENSION="massmi";}
+}
+
+// File format for Radioss 2026
+FORMAT(radioss2612) 
+{
+    HEADER("/PART/%d",_ID_);
+    CARD("%-100s",TITLE);
+    COMMENT("#  prop_ID    mat_ID subset_ID               Thick    Irigid");
+    CARD("%10d%10d%10d%20lg%10d",propertyid,materialid,subsetid,THICK,IRIGID);
+
+    if (IRIGID == 1) {
+      COMMENT("#  Node_ID   Skew_ID                Mass             Sens_ID");
+      CARD("%10d%10d%20lg%10s%10d",Node_ID,Skew_ID,Mass,_BLANK_,Sens_ID);
+      COMMENT("#                Jxx                 Jyy                 Jzz");
+      CARD("%20lg%20lg%20lg",JXX,JYY,JZZ);
+      COMMENT("#                Jxy                 Jyz                 Jxz");
+      CARD("%20lg%20lg%20lg",JXY,JYZ,JXZ);
+    }
+}
+
+// File format for Radioss 51
+FORMAT(radioss51) 
+{
+    HEADER("/PART/%d",_ID_);
+    CARD("%-100s",TITLE);
+    COMMENT("#  prop_ID    mat_ID subset_ID               Thick");
+    CARD("%10d%10d%10d%20lg",propertyid,materialid,subsetid,THICK);
+}
+
+// File format for Radioss 41
+FORMAT(radioss41) 
+{
+    HEADER("/PART/%d/%-40s",_ID_,TITLE);
+    COMMENT("#prop_ID  mat_ID  subset           Thick");
+    CARD("%8d%8d%8d%16lg",propertyid,materialid,subsetid,THICK);
+}
+

starter/source/constraints/general/rbody/checkrby.F
@@ -74,7 +74,7 @@ C   L o c a l   V a r i a b l e s
 C-----------------------------------------------
       INTEGER I,J,K,KK,N,NSL,
      .   ISPHER,IDIR,ID,ICDG,NSL_XTRA,
-     .   NRB,NUMSL_TMP,IFOUND,IM_ID(NRBYKIN),NM,IM,IACTI
+     .   NRB,NUMSL_TMP,IFOUND,IM_ID(NRBYKIN),NM,IM,IACTI,NUMSL1
       CHARACTER(LEN=NCHARTITLE)::TITR
       INTEGER, DIMENSION(:,:), ALLOCATABLE :: TABSL
       INTEGER, DIMENSION(:), ALLOCATABLE :: INDEX,IKINE1
@@ -99,9 +99,10 @@ C   NPBY(14,N) = NUMBER OF XTRA_NODE with Iflag=1
 C   NPBY(15,N) = NUMBER OF XTRA_NODE with Iflag=2
 C   NPBY(16,N) = NUMBER OF XTRA_NODE with Iflag=3
 C=======================================================================
-      IF (NUMSL > 0) THEN
-        ALLOCATE(TABSL(2,NUMSL))
-        ALLOCATE(INDEX(3*NUMSL))
+      NUMSL1 = NPBY(2,NRBYKIN) + NPBY(11,NRBYKIN) ! effective number<allocated size due to reductions
+      IF (NUMSL1 > 0) THEN
+        ALLOCATE(TABSL(2,NUMSL1))
+        ALLOCATE(INDEX(3*NUMSL1))
         TABSL=0
         INDEX=0
       END IF

starter/source/devtools/hm_reader/hm_create_rbodies_from_rigid_parts.F90
@@ -1,0 +1,59 @@
+!Copyright>        OpenRadioss
+!Copyright>        Copyright (C) 1986-2026 Altair Engineering Inc.
+!Copyright>
+!Copyright>        This program is free software: you can redistribute it and/or modify
+!Copyright>        it under the terms of the GNU Affero General Public License as published by
+!Copyright>        the Free Software Foundation, either version 3 of the License, or
+!Copyright>        (at your option) any later version.
+!Copyright>
+!Copyright>        This program is distributed in the hope that it will be useful,
+!Copyright>        but WITHOUT ANY WARRANTY; without even the implied warranty of
+!Copyright>        MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+!Copyright>        GNU Affero General Public License for more details.
+!Copyright>
+!Copyright>        You should have received a copy of the GNU Affero General Public License
+!Copyright>        along with this program.  If not, see <https://www.gnu.org/licenses/>.
+!Copyright>
+!Copyright>
+!Copyright>        Commercial Alternative: Altair Radioss Software
+!Copyright>
+!Copyright>        As an alternative to this open-source version, Altair also offers Altair Radioss
+!Copyright>        software under a commercial license.  Contact Altair to discuss further if the
+!Copyright>        commercial version may interest you: https://www.altair.com/radioss/.
+!||====================================================================
+!||    hm_create_rbodies_from_rigid_parts_mod   ../starter/source/devtools/hm_reader/hm_create_rbodies_from_rigid_parts.F90
+!||====================================================================
+      module hm_create_rbodies_from_rigid_parts_mod
+        implicit none
+      contains
+! ======================================================================================================================
+!                                                   procedures
+! ======================================================================================================================
+!! \brief create /RBODY for RIGID parts (/PART with Irigid)
+!! \details This routine create a /RBODY for each /PART with Irigid .
+!||====================================================================
+!||    hm_create_rbodies_from_rigid_parts    ../starter/source/devtools/hm_reader/hm_create_rbodies_from_rigid_parts.F90
+!||--- calls      -----------------------------------------------------
+!||====================================================================
+        subroutine hm_create_rbodies_from_rigid_parts(NB_NEWRBODIES,NEW_RBODY_TO_PART,NEW_RBODY_ID)
+! ----------------------------------------------------------------------------------------------------------------------
+!                                                   Modules
+! ----------------------------------------------------------------------------------------------------------------------
+! ----------------------------------------------------------------------------------------------------------------------
+!                                                   Implicit none
+! ----------------------------------------------------------------------------------------------------------------------
+          implicit none
+! ----------------------------------------------------------------------------------------------------------------------
+!                                                   Arguments
+! ----------------------------------------------------------------------------------------------------------------------
+          integer, intent(in) :: NB_NEWRBODIES
+          integer, dimension(NB_NEWRBODIES), intent(out) :: NEW_RBODY_TO_PART
+          integer, dimension(NB_NEWRBODIES), intent(out) :: NEW_RBODY_ID
+! ----------------------------------------------------------------------------------------------------------------------
+! ----------------------------------------------------------------------------------------------------------------------
+!                                                   Body
+! ----------------------------------------------------------------------------------------------------------------------
+          call cpp_create_rbodies_from_rigid_parts(NEW_RBODY_TO_PART,NEW_RBODY_ID)
+! ----------------------------------------------------------------------------------------------------------------------
+        end subroutine hm_create_rbodies_from_rigid_parts
+      end module hm_create_rbodies_from_rigid_parts_mod

starter/source/devtools/hm_reader/hm_evaluate_rbodies_from_rigid_parts.F90
@@ -1,0 +1,58 @@
+!Copyright>        OpenRadioss
+!Copyright>        Copyright (C) 1986-2026 Altair Engineering Inc.
+!Copyright>
+!Copyright>        This program is free software: you can redistribute it and/or modify
+!Copyright>        it under the terms of the GNU Affero General Public License as published by
+!Copyright>        the Free Software Foundation, either version 3 of the License, or
+!Copyright>        (at your option) any later version.
+!Copyright>
+!Copyright>        This program is distributed in the hope that it will be useful,
+!Copyright>        but WITHOUT ANY WARRANTY; without even the implied warranty of
+!Copyright>        MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+!Copyright>        GNU Affero General Public License for more details.
+!Copyright>
+!Copyright>        You should have received a copy of the GNU Affero General Public License
+!Copyright>        along with this program.  If not, see <https://www.gnu.org/licenses/>.
+!Copyright>
+!Copyright>
+!Copyright>        Commercial Alternative: Altair Radioss Software
+!Copyright>
+!Copyright>        As an alternative to this open-source version, Altair also offers Altair Radioss
+!Copyright>        software under a commercial license.  Contact Altair to discuss further if the
+!Copyright>        commercial version may interest you: https://www.altair.com/radioss/.
+!||====================================================================
+!||    hm_evaluate_rbodies_from_rigid_parts_mod   ../starter/source/devtools/hm_reader/hm_evaluate_rbodies_from_rigid_parts.F90
+!||====================================================================
+      module hm_evaluate_rbodies_from_rigid_parts_mod
+        implicit none
+      contains
+! ======================================================================================================================
+!                                                   procedures
+! ======================================================================================================================
+!! \brief create /RBODY for RIGID parts (/PART with Irigid)
+!! \details This routine create a /RBODY for each /PART with Irigid .
+!||====================================================================
+!||    hm_evaluate_rbodies_from_rigid_parts    ../starter/source/devtools/hm_reader/hm_evaluate_rbodies_from_rigid_parts.F90
+!||--- calls      -----------------------------------------------------
+!||====================================================================
+        subroutine hm_evaluate_rbodies_from_rigid_parts(NPART,NBRBODIES_PER_PART)
+! ----------------------------------------------------------------------------------------------------------------------
+!                                                   Modules
+! ----------------------------------------------------------------------------------------------------------------------
+! ----------------------------------------------------------------------------------------------------------------------
+!                                                   Implicit none
+! ----------------------------------------------------------------------------------------------------------------------
+          implicit none
+! ----------------------------------------------------------------------------------------------------------------------
+!                                                   Arguments
+! ----------------------------------------------------------------------------------------------------------------------
+          integer, intent(in) :: NPART
+          integer, dimension(NPART), intent(out) :: NBRBODIES_PER_PART
+! ----------------------------------------------------------------------------------------------------------------------
+! ----------------------------------------------------------------------------------------------------------------------
+!                                                   Body
+! ----------------------------------------------------------------------------------------------------------------------
+          call cpp_evaluate_rbodies_number_from_rigid_parts(NBRBODIES_PER_PART)
+! ----------------------------------------------------------------------------------------------------------------------
+        end subroutine hm_evaluate_rbodies_from_rigid_parts
+      end module hm_evaluate_rbodies_from_rigid_parts_mod

starter/source/elements/initia/initia.F
@@ -984,7 +984,7 @@ C
      G        IXR       , IMERGE2    ,IADMERGE2  ,NEL       ,DEFAULTS  ,
      H        GLOB_THERM,IBEAM_VECTOR,RBEAM_VECTOR)
 C
-       ELSEIF( MTN == 13) THEN
+       ELSEIF( IRIGID_MAT>0) THEN
 C Rigid material
            LFT=1
            LLT=NEL

starter/source/loads/general/grav/hm_preread_grav.F
@@ -33,7 +33,7 @@ Copyright>        commercial version may interest you: https://www.altair.com/ra
 !||    hm_option_read_mod     ../starter/share/modules1/hm_option_read_mod.F
 !||    submodel_mod           ../starter/share/modules1/submodel_mod.F
 !||====================================================================
-      SUBROUTINE HM_PREREAD_GRAV(NUM    ,IGRNOD,LSUBMODEL)
+      SUBROUTINE HM_PREREAD_GRAV(NUM    ,IGRNOD,LSUBMODEL,N_NRBODY)
 C-----------------------------------------------
 C   M o d u l e s
 C-----------------------------------------------
@@ -56,6 +56,7 @@ C-----------------------------------------------
 C-----------------------------------------------
       TYPE (GROUP_)  , DIMENSION(NGRNOD)  :: IGRNOD
       TYPE(SUBMODEL_DATA),INTENT(IN)::LSUBMODEL(*)
+      INTEGER, INTENT(IN) :: N_NRBODY
 C-----------------------------------------------
 C   L o c a l   V a r i a b l e s
 C-----------------------------------------------
@@ -91,6 +92,7 @@ C--------------------------------------------------
 C--------------------------------------------------
         IF (IGU /= 0) THEN
           IGS = NGR2USRN(IGU,IGRNOD,NGRNOD,NN)
+          NN = NN + N_NRBODY
         ELSE
           NN = NUMNOD
         ENDIF

commit 23a3b53c88b417e70fe73053b9d5ff43cd0c234c

one more regression in 9bc12a8f16318b336ae3393971de9ab973e57cd8

starter/source/constraints/general/rbody/hm_read_rbody.F
@@ -316,7 +316,8 @@ C
         NPBY(11,NRB)=K
         NPBY(6,NRB)=ID
         M = NPBY(1,NRB)
-        NSL = NODGRNR6(M,IGU,IGS,LPBY(K+1),IGRNOD,ITABM1,MESS,ID)
+        NSL = 0
+        IF (IGU>0) NSL = NODGRNR6(M,IGU,IGS,LPBY(K+1),IGRNOD,ITABM1,MESS,ID)
         IF (NRB>NRBYKIN0) THEN
           CALL RBODY_PART_MODIF(NRBYKIN ,NNPBY ,NPBY  ,SLRBODY ,LPBY  ,          
      .                           NUMNOD ,NRB   ,NSL   ,LPBY(K+1:K+NSL),

starter/source/constraints/general/rbody/rbody_part_modif.F90
@@ -234,7 +234,7 @@
 ! ======================================================================================================================
           jt_m(1:3)=0
           jr_m(1:3)=0
-          isk_m = iskew(isl(1))
+          isk_m = max(1,iskew(isl(1)))
           do i=1,nsl
             ns = isl(i)
             ic = icode(ns)
@@ -254,12 +254,6 @@
               jt(1) = ict/4
               jt(2) = (ict-4*jt(1))/2
               jt(3) = ict-4*jt(1)-2*jt(2)
-              if ((jt(1)+jt_m(1))>2) jr_m(2:3) = 1
-              if ((jt(2)+jt_m(2))>2) then 
-                jr_m(1) = 1
-                jr_m(3) = 1
-              end if
-              if ((jt(3)+jt_m(3))>2) jr_m(1:2) = 1
               jt_m(1:3) = max(jt_m(1:3),jt(1:3))
             end if
             if (jr_m(1)+jr_m(2)+jr_m(3)<3) then
@@ -267,6 +261,12 @@
               jr(2) = (icr-4*jr(1))/2
               jr(3) = icr-4*jr(1)-2*jr(2)
               jr_m(1:3) = max(jr_m(1:3),jr(1:3))
+              if ((jt(1)+jt_m(1))==2) jr_m(2:3) = 1
+              if ((jt(2)+jt_m(2))==2) then 
+                jr_m(1) = 1
+                jr_m(3) = 1
+              end if
+              if ((jt(3)+jt_m(3))==2) jr_m(1:2) = 1
             end if
             icode(ns) = 0
           end do